801812a9efc2a111848faa43a80b0f1f6a06145d
francois <francois.armand@normation.com>
2011-11-22 23:39:24 +0100

    Issue #2075: add a generic matcher for UUIDs

---
+++ utils/src/main/scala/com/normation/utils/UuidRegex.scala
@@ -0,0 +1,11 @@
+package com.normation.utils
+import java.util.regex.Pattern
+
+object UuidRegex {
+
+  val stringPattern = """[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}"""
+    
+  val pattern = Pattern.compile(stringPattern)
+ 
+  def isValid(candidate:String) : Boolean = pattern.matcher(candidate).matches
+}
