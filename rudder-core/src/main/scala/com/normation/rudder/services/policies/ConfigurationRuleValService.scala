846b4eb1855058ff5eef0aa2919e6715cc5a6da4
Nicolas Charles <nicolas.charles@normation.com>
2012-04-20 15:50:07 +0200

    Fixes #2456 : don't apply disabled PI when they are disabled

--- rudder-core/src/main/scala/com/normation/rudder/services/policies/ConfigurationRuleValService.scala
+++ rudder-core/src/main/scala/com/normation/rudder/services/policies/ConfigurationRuleValService.scala
@@ -35,6 +35,7 @@
 package com.normation.rudder.services.policies
 
 import com.normation.rudder.domain.policies.ConfigurationRuleId
+import com.normation.rudder.domain.policies.PolicyInstanceId
 import com.normation.rudder.domain.policies.ConfigurationRuleVal
 import net.liftweb.common._
 import com.normation.cfclerk.services.PolicyPackageService
@@ -44,6 +45,7 @@ import com.normation.rudder.domain.policies.PolicyInstanceContainer
 import com.normation.cfclerk.domain._
 import com.normation.cfclerk.exceptions._
 import com.normation.utils.Control.sequence
+import com.normation.utils.Control.sequenceEmptyable
 
 trait ConfigurationRuleValService {
   def findConfigurationRuleVal(configurationRuleId:ConfigurationRuleId) : Box[ConfigurationRuleVal]
@@ -58,33 +60,43 @@ class ConfigurationRuleValServiceImpl (
   val variableBuilderService: VariableBuilderService
 ) extends ConfigurationRuleValService with Loggable {
  
+  private[this] def getContainer(piId : PolicyInstanceId, configurationRuleId:ConfigurationRuleId) : Box[PolicyInstanceContainer]= {
+    policyInstanceRepo.getPolicyInstance(piId) match {
+      case e:EmptyBox => e
+      case Full(pi) if !(pi.isActivated) => Empty
+      case Full(pi) if (pi.isActivated) =>
+        policyInstanceRepo.getUserPolicyTemplate(piId) match {
+          case e:EmptyBox => e
+          case Full(upt) if !(upt.isActivated) => Empty
+          case Full(upt) if upt.isActivated =>
+            for {
+              policyPackage <- policyPackageService.getPolicy(PolicyPackageId(upt.referencePolicyTemplateName,pi.policyTemplateVersion))
+              varSpecs = policyPackage.rootSection.getAllVariables ++ policyPackage.systemVariableSpecs :+ policyPackage.trackerVariableSpec
+              vared <- variableBuilderService.buildVariables(varSpecs, pi.parameters)
+              trackerVariable <- vared.get(policyPackage.trackerVariableSpec.name)
+              otherVars = vared - policyPackage.trackerVariableSpec.name
+              } yield {
+                logger.debug("Creating a PolicyInstanceContainer %s from the configurationRuleId %s".format(upt.referencePolicyTemplateName, configurationRuleId))
+              
+                PolicyInstanceContainer(
+                    policyPackage.id,
+                    upt.id,
+                    pi.id,
+                    pi.priority,
+                    policyPackage.trackerVariableSpec.toVariable(trackerVariable.values),
+                    otherVars
+                )
+              }
+        }
+    }
+  } 
+  
   def findConfigurationRuleVal(configurationRuleId:ConfigurationRuleId) : Box[ConfigurationRuleVal] = {
     for {
       cr <- configurationRuleRepo.get(configurationRuleId)
       target <- Box(cr.target) ?~! "Can not fetch configuration rule values for configuration rule with id %s. The reference target is not defined and I can not build a ConfigurationRuleVal for a not fully defined configuration rule".format(configurationRuleId)
       pisId = cr.policyInstanceIds.toSeq
-      containers <- sequence(pisId) { piId => {
-        for {
-        pi <- policyInstanceRepo.getPolicyInstance(piId) ?~! "Can not fetch policy instance %s for configuration rule with id %s. ".format(piId.value, configurationRuleId)
-        upt <- policyInstanceRepo.getUserPolicyTemplate(piId)  ?~! "Can not fetch policy template %s for configuration rule with id %s. ".format(piId.value, configurationRuleId)
-        policyPackage <- policyPackageService.getPolicy(PolicyPackageId(upt.referencePolicyTemplateName,pi.policyTemplateVersion))
-        varSpecs = policyPackage.rootSection.getAllVariables ++ policyPackage.systemVariableSpecs :+ policyPackage.trackerVariableSpec
-        vared <- variableBuilderService.buildVariables(varSpecs, pi.parameters)
-        trackerVariable <- vared.get(policyPackage.trackerVariableSpec.name)
-        otherVars = vared - policyPackage.trackerVariableSpec.name
-        } yield {
-          logger.debug("Creating a PolicyInstanceContainer %s from the configurationRuleId %s".format(upt.referencePolicyTemplateName, configurationRuleId))
-        
-          PolicyInstanceContainer(
-          		policyPackage.id,
-          		upt.id,
-          		pi.id,
-          		pi.priority,
-          		policyPackage.trackerVariableSpec.toVariable(trackerVariable.values),
-          		otherVars
-          )
-        }
-      } }
+      containers <- sequenceEmptyable(pisId) { piId => getContainer(piId, configurationRuleId) }
     } yield {
       ConfigurationRuleVal(
         cr.id,
