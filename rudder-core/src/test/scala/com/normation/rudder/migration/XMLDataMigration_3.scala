1329c8e639435ff6852a83dd9ca3ae1d234e230f
Vincent Membré <vincent.membre@normation.com>
2012-07-20 17:40:12 +0200

    Ref #2639 add sql file +licence to file

--- rudder-core/src/test/scala/com/normation/rudder/migration/XMLDataMigration_3.scala
+++ rudder-core/src/test/scala/com/normation/rudder/migration/XMLDataMigration_3.scala
@@ -1,6 +1,6 @@
 /*
 *************************************************************************************
-* Copyright 2011 Normation SAS
+* Copyright 2012 Normation SAS
 *************************************************************************************
 *
 * This program is free software: you can redistribute it and/or modify
