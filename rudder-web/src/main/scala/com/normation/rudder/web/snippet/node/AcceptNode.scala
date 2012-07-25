797412da7eef935583c824fa6952490db931ec02
Jean V <jean.vilver@normation.com>
2012-07-25 17:15:58 +0200

    Fixes #2689 Arrange the buttons on Accept new node view

--- rudder-web/src/main/scala/com/normation/rudder/web/snippet/node/AcceptNode.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/snippet/node/AcceptNode.scala
@@ -351,7 +351,7 @@ class AcceptNode {
       ),
       "accept" -> {if (seq.size > 0 ) { SHtml.ajaxButton("Accept into Rudder", {
         () =>  showConfirmPopup(acceptTemplate, "confirmPopup")
-      }) } else NodeSeq.Empty},
+      }) % ("style", "width:140px")} else NodeSeq.Empty},
       "refuse" -> {if (seq.size > 0 ) { SHtml.ajaxButton("Refuse", {
         () => showConfirmPopup(refuseTemplate, "refusePopup")
       }) } else NodeSeq.Empty},
