ec21d20d09eae352e9410e087517fd2f985c7cac
Vincent Membré <vincent.membre@normation.com>
2012-07-31 18:48:31 +0200

    Ref #2581 add test to Json research

--- rudder-core/src/test/scala/com/normation/rudder/domain/queries/CmdbQueryTest.scala
+++ rudder-core/src/test/scala/com/normation/rudder/domain/queries/CmdbQueryTest.scala
@@ -7,6 +7,9 @@ import org.specs2.mutable._
 import org.specs2.matcher._
 import org.specs2.runner.JUnitRunner
 import net.liftweb.common._
+import com.unboundid.ldap.sdk._
+import com.normation.ldap.sdk._
+import BuildFilter._
 
 @RunWith(classOf[JUnitRunner])
 class CmdbQueryTest extends Specification {
@@ -31,6 +34,24 @@ class CmdbQueryTest extends Specification {
         case e:EmptyBox =>failure("Invalid parsing")
         case Full(_) => success
       }
+    }
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class JsonQueryTest extends Specification {
+
+  "JsonComparator " should {
+    "Convert a search request to a valid json request on one attribute " in {
+      JsonComparator("attribute").buildFilter("key",Equals,"value") must beEqualTo(SUB("attribute",null,("\"key\":\"value\""::Nil).toArray,null)) 
+    }
+    
+    "Convert a search request to a valid json request on 2 attributes" in {
+      JsonComparator("attribute","=").buildFilter("key.key2",Equals,"value=value2") must beEqualTo(SUB("attribute",null,("\"key\":\"value\""::"\"key2\":\"value2\""::Nil).toArray,null)) 
+    }
+    
+    "Convert a search request to a valid json request on one numeric attribute" in {
+      JsonComparator("attribute","",true).buildFilter("key",Equals,"1") must beEqualTo(SUB("attribute",null,("\"key\":1"::Nil).toArray,null)) 
     }
   }
 }
