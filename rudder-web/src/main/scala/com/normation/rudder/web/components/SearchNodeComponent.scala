3855fad86e47c28c6497d9b4dc096697021e775c
Jean V <jean.vilver@normation.com>
2012-08-02 13:39:28 +0200

    Fixes #2760 Align text with checkbox in group form

--- rudder-web/src/main/scala/com/normation/rudder/web/components/SearchNodeComponent.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/components/SearchNodeComponent.scala
@@ -204,15 +204,15 @@ class SearchNodeComponent(
     def displayQuery(html: NodeSeq ) : NodeSeq = {
       val Query(otName,comp, criteria) = query.get
       SHtml.ajaxForm(bind("query", html,
-          "typeQuery" ->  SHtml.checkbox(rType==NodeAndPolicyServerReturnType, { value:Boolean =>
+          "typeQuery" ->  <label>Include policy servers: {SHtml.checkbox(rType==NodeAndPolicyServerReturnType, { value:Boolean =>
                 if (value) 
                   rType = NodeAndPolicyServerReturnType 
                 else 
                   rType = NodeReturnType}
-              , ("class" , "checkbox")),
+              )}</label>,
           "composition" -> SHtml.radio(Seq("And", "Or"), Full(if(comp == Or) "Or" else "And"), {value:String => 
           composition = CriterionComposition.parse(value).getOrElse(And) //default to AND on unknow composition string
-        }, ("class", "radio")).flatMap(e => <span class="compositionRadio">{e.xhtml} {e.key.toString}</span>),
+        }, ("class", "radio")).flatMap(e => <label>{e.xhtml} <span class="radioTextLabel">{e.key.toString}</span></label>),
         "lines" -> {(ns: NodeSeq) => 
           /*
            * General remark :
