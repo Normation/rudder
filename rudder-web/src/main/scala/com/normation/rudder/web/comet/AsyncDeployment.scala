becb2527a77a0e3a13e7993411dc2edd4a3e1a40
Vincent Membré <vincent.membre@normation.com>
2012-11-12 11:44:28 +0100

    Fixes #2982 : fix display of pending modifications

--- rudder-web/src/main/scala/com/normation/rudder/web/comet/AsyncDeployment.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/comet/AsyncDeployment.scala
@@ -243,7 +243,7 @@ class AsyncDeployment extends CometActor with CometListener with Loggable {
       </div>
       <div class="simplemodal-content">
        <br />
-       <div class="popupContent"/>
+       <div class="popupContent ui-widget ui-widget-content"/>
        <hr class="spacer" />
     </div>
     <div class="simplemodal-bottom">
