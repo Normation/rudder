f9e25502f3fdbef3cf6565e21e27a63309ee1a84
fanf <francois.armand@normation.com>
2012-11-14 17:24:42 +0100

    Fixes #2990: allow to download directives/rules/groups as zip from Rudder UI even if the REST API is restricted to localhost

--- rudder-web/src/main/scala/com/normation/rudder/web/snippet/administration/Archives.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/snippet/administration/Archives.scala
@@ -255,7 +255,7 @@ class Archives extends DispatchSnippet with Loggable {
       selectedCommitId match {
         case None    => error(Empty, "A valid archive must be chosen")
         case Some(commit) => 
-          S.redirectTo("/api/archives/zip/%s/%s".format(downloadRestAction, commit.value))
+          S.redirectTo("/secure/administration/archiveManagement/zip/%s/%s".format(downloadRestAction, commit.value))
       }
       
     }
