f2b3855ce5b237cf868dcadc6acdfc1b2f17fa16
Arthur Anglade <arthur.anglade@normation.com>
2012-12-26 15:36:21 +0100

    fixes #2930 Improve group display

--- rudder-web/src/main/scala/com/normation/rudder/web/snippet/node/Groups.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/snippet/node/Groups.scala
@@ -235,7 +235,7 @@ class Groups extends StatefulSnippet with SpringExtendableSnippet[Groups] with L
       //build jstree and
       //init bind callback to move
       JsRaw("""
-        buildGroupTree('#%1$s','%5$s', '%4$s', 'off');
+        buildGroupTree('#%1$s','%5$s', '%4$s', 'off', true);
         $('#%1$s').bind("move_node.jstree", function (e,data) {
           var sourceCatId = $(data.rslt.o).attr("catId");
           var sourceGroupId = $(data.rslt.o).attr("groupId");
