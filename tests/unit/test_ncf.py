a7f1b89119e78d05ec485fe67884aa6b2bd004a7
Jonathan Clarke <jonathan.clarke@normation.com>
2013-12-02 18:31:56 +0100

    Add method to parse CFEngine bundles and abstract method calls in ncf.py (and associated tests)

--- tests/unit/test_ncf.py
+++ tests/unit/test_ncf.py
@@ -2,12 +2,36 @@
 
 import unittest
 import ncf
+import os.path
+import subprocess
 
 class TestNcf(unittest.TestCase):
 
   def setUp(self):
-    self.technique_content = open('test_technique.cf').read()
-    self.generic_method_content = open('test_generic_method.cf').read()
+    self.test_technique_file = os.path.realpath('test_technique.cf')
+    self.test_generic_method_file = 'test_generic_method.cf'
+    self.technique_content = open(self.test_technique_file).read()
+    self.generic_method_content = open(self.test_generic_method_file).read()
+
+  #####################################
+  # Generic tests for parsing .cf files
+  #####################################
+
+  def test_parse_bundlefile_empty(self):
+    """Attempting to parse an empty string should raise an exception"""
+    self.assertRaises(Exception, ncf.parse_bundlefile_metadata, "")
+
+  def test_parse_bundlefile_incomplete(self):
+    """Attempting to parse a bundle file with metadata after the bundle agent declaration should raise an exception"""
+    self.assertRaises(Exception, ncf.parse_bundlefile_metadata, """# @name A name
+                                      bundle agent thingy {
+                                      }
+                                      # @description bla bla
+                                      # @version 1.0""")
+
+  #########################
+  # Technique parsing tests
+  #########################
 
   def test_parse_technique(self):
     """Parsing should return a dict with all defined technique tags"""
@@ -21,24 +45,33 @@ class TestNcf(unittest.TestCase):
     self.assertEqual(metadata['description'], "This meta-Technique is a sample only, allowing for testing.")
     self.assertEqual(metadata['version'], "0.1")
 
-  def test_parse_bundlefile_empty(self):
-    """Attempting to parse an empty string should raise an exception"""
-    with self.assertRaises(Exception):
-      ncf.parse_bundlefile_metadata("")
-
-  def test_parse_bundlefile_incomplete(self):
-    """Attempting to parse a bundle file with metadata after the bundle agent declaration should raise an exception"""
-    with self.assertRaises(Exception):
-      ncf.parse_bundlefile_metadata("""# @name A name
-                                      bundle agent thingy {
-                                      }
-                                      # @description bla bla
-                                      # @version 1.0""")
+  ##############################
+  # Generic method parsing tests
+  ##############################
 
   def test_parse_generic_method(self):
     """Parsing a generic method should return a dict with all defined generic_method tags"""
     metadata = ncf.parse_generic_method_metadata(self.generic_method_content)
     self.assertEqual(sorted(metadata.keys()), sorted(ncf.tags["generic_method"]))
+
+  ###########################################################
+  # Tests to obtain the generic methods that a Technique uses
+  ###########################################################
+
+  def test_parse_technique_generic_method_calls_nonexistant_file(self):
+    """Attempting to parse a non existant file should return an exception"""
+    self.assertRaises(Exception, ncf.parse_technique_methods, "/dev/nonexistant")
+
+  def test_parse_technique_generic_method_calls(self):
+    """Parsing a technique should return a list of it's generic method calls"""
+    bundle_calls = ncf.parse_technique_methods(self.test_technique_file)
+    expected = [  { 'method_name': 'package_install_version', 'args': ['${bla.apache_package_name}', '2.2.11'], 'class_context': 'any' },
+                  { 'method_name': 'service_restart', 'args': ['${bla.apache_package_name}'], 'class_context': 'cfengine' },
+                  { 'method_name': 'package_install', 'args': ['nano'], 'class_context': 'cfengine' },
+                  { 'method_name': 'shouldnt_happen_but_want_to_parse_anyway', 'class_context': 'cfengine' },
+               ]
+    self.assertEqual(bundle_calls, expected)
+
 
 if __name__ == '__main__':
   unittest.main()
--- tools/ncf.py
+++ tools/ncf.py
@@ -8,6 +8,9 @@
 # module must support
 
 import re
+import subprocess
+import json
+import os.path
 
 tags = {}
 tags["generic_method"] = ["name", "class_prefix"]
@@ -39,5 +42,50 @@ def parse_bundlefile_metadata(content, bundle_type):
   if sorted(res.keys()) != sorted(tags[bundle_type]):
     missing_keys = [mkey for mkey in tags[bundle_type] if mkey not in set(res.keys())]
     raise Exception("One or more metadata tags not found before the bundle agent declaration (" + ", ".join(missing_keys) + ")")
+
+  return res
+
+def parse_technique_methods(technique_file):
+  res = []
+
+  # Check file exists
+  if not os.path.exists(technique_file):
+    raise Exception("No such file: " + technique_file)
+
+  out = subprocess.check_output(["cf-promises", "-pjson", "-f", technique_file])
+  promises = json.loads(out)
+
+  # Sanity check: if more than one bundle, this is a weird file and I'm quitting
+  if len(promises['bundles']) != 1:
+    raise Exception("There is not exactly one bundle in this file, aborting")
+
+  # Sanity check: the bundle must be of type agent
+  if promises['bundles'][0]['bundleType'] != 'agent':
+    raise Exception("This bundle if not a bundle agent, aborting")
+
+  methods = [promiseType for promiseType in promises['bundles'][0]['promiseTypes'] if promiseType['name']=="methods"][0]['contexts']
+  #print "context = " + methods[0]['contexts']['name']
+
+  for context in methods:
+    class_context = context['name']
+
+    for method in context['promises']:
+      method_name = None
+      args = None
+
+      promiser = method['promiser']
+
+      for attribute in method['attributes']:
+        if attribute['lval'] == 'usebundle':
+          if attribute['rval']['type'] == 'functionCall':
+            method_name = attribute['rval']['name']
+            args = [arg['value'] for arg in attribute['rval']['arguments']]
+          if attribute['rval']['type'] == 'string':
+            method_name = attribute['rval']['value']
+
+      if args:
+        res.append({'class_context': class_context, 'method_name': method_name, 'args': args})
+      else:
+        res.append({'class_context': class_context, 'method_name': method_name})
 
   return res
