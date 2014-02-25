a6849fb09d650c8de1ece5bef3c65f17eeec41d0
Francois @fanf42 Armand <francois.armand@normation.com>
2014-02-25 17:14:23 +0100

    Fixes #4515: Add AIX fusion-inventory report parsing support

--- inventory-repository/src/main/scala/com/normation/inventory/ldap/core/InventoryDit.scala
+++ inventory-repository/src/main/scala/com/normation/inventory/ldap/core/InventoryDit.scala
@@ -177,6 +177,12 @@ case class InventoryDit(val BASE_DN:DN, val SOFTWARE_BASE_DN:DN, val name:String
         mod
       }
 
+      def aixModel(id:NodeId) : LDAPEntry = {
+        val mod = model(id)
+        mod += (A_OC,OC.objectClassNames(OC_AIX_NODE).toSeq:_*)
+        mod
+      }
+
       def dn(uuid:String) = new DN(this.rdn(uuid), servers.dn)
 
       def idFromDN(dn:DN) : Box[NodeId] = {
