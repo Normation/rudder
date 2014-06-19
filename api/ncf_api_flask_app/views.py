132b38f9a00a94773c51d04b67985adda7e21a62
Vincent Membré <vincent.membre@normation.com>
2014-06-19 14:22:28 +0200

    Fixes #5083: ncf api auth, using Rudder api auth and cookie

--- api/ncf_api_flask_app/views.py
+++ api/ncf_api_flask_app/views.py
@@ -1,5 +1,6 @@
 from ncf_api_flask_app import app
 from flask import Flask, jsonify, request, abort
+from functools import wraps
 import requests
 import os
 import sys
@@ -10,7 +11,52 @@ sys.path[0:0] = [NCF_TOOLS_DIR]
 
 import ncf
 
+def check_auth(f):
+  """Method to use before each API call to check authentication"""
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    auth = get_auth()
+    if auth.status_code == 401:
+      return auth
+    return f(*args, **kwargs)
+  return decorated
+
+def check_authentication_from_rudder(auth_request):
+  """Send an authentication request to Rudder"""
+  # We skip ssl certificate verification, since rudder and ncf-api are on the same domain and same virtualhost
+  auth_result = requests.get('https://localhost/rudder/authentication', cookies =  request.cookies, verify = False)
+
+  auth_response = jsonify( auth_result.json() )
+  auth_response.status_code = auth_result.status_code
+
+  return auth_response
+
+
+def get_authentication_modules():
+  """Find all available authentication modules"""
+  # List of all authentication modules
+  authentication_modules = { "Rudder" : check_authentication_from_rudder }
+  # Name of all available modules, should read from a file or ncf path. only Rudder available for now
+  available_modules_name = ["Rudder"]
+
+  available_modules = [ module for module_name, module in authentication_modules.iteritems() if module_name in available_modules_name ]
+  return available_modules
+
+@app.route('/api/auth', methods = ['GET'])
+def get_auth():
+  """ Check authentication, should look for available modules and pass authentication on them"""
+  for authentication_module in get_authentication_modules():
+    authentication_result = authentication_module(request)
+    if authentication_result.status_code == 200:
+      # Authentication success return it
+      return authentication_result
+  # all authentication have failed, return an error
+  error = jsonify ({ "error" : "Could not authenticate with ncf API"})
+  error.status_code = 401
+  return  error
+
 @app.route('/api/techniques', methods = ['GET'])
+@check_auth
 def get_techniques():
   """Get all techniques from ncf folder passed as parameter, or default ncf folder if not defined"""
   # We need to get path from url params, if not present put "" as default value
@@ -24,6 +70,7 @@ def get_techniques():
   return resp
 
 @app.route('/api/generic_methods', methods = ['GET'])
+@check_auth
 def get_generic_methods():
   """Get all generic methods from ncf folder passed as parameter, or default ncf folder if not defined"""
   # We need to get path from url params, if not present put "" as default value
@@ -37,6 +84,7 @@ def get_generic_methods():
   return resp
 
 @app.route('/api/techniques', methods = ['POST', "PUT"])
+@check_auth
 def create_technique():
   # Get data in JSON
   # technique is a mandatory parameter, abort if not present
@@ -57,7 +105,9 @@ def create_technique():
     
   return jsonify( technique ), 201
 
+
 @app.route('/api/techniques/<string:bundle_name>', methods = ['DELETE'])
+@check_auth
 def delete_technique(bundle_name):
     
   if "path" in request.args:
