5eb0bb0250c5e790cd47bf9ba53b20e5be8bc876
Jonathan Clarke <jonathan.clarke@normation.com>
2014-12-05 12:33:58 +0100

    Fixes #5929: nbsp test is failing in ncf because it checks to many files

--- tests/style/tests/nbsp_is_not_allowed.sh
+++ tests/style/tests/nbsp_is_not_allowed.sh
@@ -4,13 +4,22 @@ set -e
 
 # NBSP breaks scripts and cfengine code, they should never appear
 
-ALL_TESTS=`find ${NCF_TREE}/.. -type f | grep -v flask`
+# Build fine arguments
+FIND_ARGS="-type f -not -wholename */.git/* -not -wholename */api/flask/* -not -name *.png -not -name *.eot -not -name *.ttf -not -name *.woff -not -name *.otf"
+
+# Automatically exclude anything from the .gitignore file
+while read line
+do
+  export FIND_ARGS="${FIND_ARGS} -not -wholename */${line}"
+done < ${NCF_TREE}/../.gitignore
+
+ALL_TESTS=`find ${NCF_TREE}/.. ${FIND_ARGS}`
 
 ERRORS=0
 for file in ${ALL_TESTS}
 do
   # allow nbsp in comments
-  if grep -r -P '^[^#]*\xA0' ${file} > /dev/null; then
+  if grep -P '^[^#]*\xA0' ${file} > /dev/null; then
     ERRORS=`expr ${ERRORS} + 1`
     echo "Test ${file} has a non breaking space in it"
   fi
