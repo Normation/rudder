f29acc85414c140ef9ec8eee0614fc0e01ca315b
Vincent Membré <vincent.membre@normation.com>
2014-12-17 15:41:37 +0100

    Fixes #5837: Add event log for compliance mode and heart period
    Conflicts:
    	rudder-core/src/main/scala/com/normation/rudder/domain/eventlog/EventLogCategories.scala
    	rudder-web/src/main/resources/eventLogTypeNames.properties

--- rudder-core/src/main/scala/com/normation/rudder/domain/eventlog/ModifyGlobalProperty.scala
+++ rudder-core/src/main/scala/com/normation/rudder/domain/eventlog/ModifyGlobalProperty.scala
@@ -9,6 +9,25 @@ sealed trait ModifyGlobalProperty extends EventLog {
   def propertyName : String
 }
 
+object ModifyGlobalProperty {
+
+   def apply(eventType: ModifyGlobalPropertyEventType, eventDetails : EventLogDetails) : ModifyGlobalProperty  = {
+    eventType match {
+      case ModifySendServerMetricsEventType => ModifySendServerMetrics(eventDetails)
+      case ModifyComplianceModeEventType => ModifyComplianceMode(eventDetails)
+      case ModifyHeartbeatPeriodEventType => ModifyHeartbeatPeriod(eventDetails)
+    }
+   }
+}
+
+object ModifyGlobalPropertyEventLogsFilter {
+  final val eventList : List[EventLogFilter] =
+      ModifySendServerMetrics ::
+      ModifyComplianceMode ::
+      ModifyHeartbeatPeriod ::
+      Nil
+}
+
 final case class ModifySendServerMetrics (
     override val eventDetails : EventLogDetails
 ) extends ModifyGlobalProperty with HashcodeCaching {
@@ -23,19 +42,30 @@ object ModifySendServerMetrics extends EventLogFilter {
   override def apply(x : (EventLogType, EventLogDetails)) : ModifySendServerMetrics = ModifySendServerMetrics(x._2)
 }
 
-
-object ModifyGlobalProperty {
-
-   def apply(eventType: ModifyGlobalPropertyEventType, eventDetails : EventLogDetails) : ModifyGlobalProperty  = {
-    eventType match {
-      case ModifySendServerMetricsEventType => ModifySendServerMetrics(eventDetails)
-    }
-   }
+final case class ModifyComplianceMode (
+    override val eventDetails : EventLogDetails
+) extends ModifyGlobalProperty with HashcodeCaching {
+  override val cause = None
+  override val eventType = ModifyComplianceMode.eventType
+  override val propertyName = "Compliance mode"
 }
 
+object ModifyComplianceMode extends EventLogFilter {
+  override val eventType = ModifyComplianceModeEventType
 
-object ModifyGlobalPropertyEventLogsFilter {
-  final val eventList : List[EventLogFilter] =
-      ModifySendServerMetrics ::
-      Nil
+  override def apply(x : (EventLogType, EventLogDetails)) : ModifyComplianceMode = ModifyComplianceMode(x._2)
+}
+
+final case class ModifyHeartbeatPeriod (
+    override val eventDetails : EventLogDetails
+) extends ModifyGlobalProperty with HashcodeCaching {
+  override val cause = None
+  override val eventType = ModifyHeartbeatPeriod.eventType
+  override val propertyName = "Heartbeat period"
+}
+
+object ModifyHeartbeatPeriod extends EventLogFilter {
+  override val eventType = ModifyHeartbeatPeriodEventType
+
+  override def apply(x : (EventLogType, EventLogDetails)) : ModifyHeartbeatPeriod = ModifyHeartbeatPeriod(x._2)
 }
