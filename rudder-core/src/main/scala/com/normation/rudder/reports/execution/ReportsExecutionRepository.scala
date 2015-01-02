6afb02d55ab23558c56c3e9a633fe89b9107e456
Francois @fanf42 Armand <francois.armand@normation.com>
2015-01-02 14:56:44 +0100

    Fixes #6063: Performance of retrieving last node run reports time impact both compliance and list of node screen

--- rudder-core/src/main/scala/com/normation/rudder/reports/execution/ReportsExecutionRepository.scala
+++ rudder-core/src/main/scala/com/normation/rudder/reports/execution/ReportsExecutionRepository.scala
@@ -37,7 +37,7 @@ package com.normation.rudder.reports.execution
 import com.normation.inventory.domain.NodeId
 import net.liftweb.common._
 import org.joda.time.DateTime
-
+import com.normation.rudder.repository.CachedRepository
 
 /**
  * Service for reading or storing execution of Nodes
@@ -71,4 +71,59 @@ trait WoReportsExecutionRepository {
    */
   def updateExecutions(executions : Seq[AgentRun]) : Box[Seq[AgentRun]]
 
+}
+
+/**
+ * A cached version of the service that only look in the underlying data (expected to
+ * be slow) when no cache available.
+ */
+class CachedReportsExecutionRepository(
+    readBackend : RoReportsExecutionRepository
+  , writeBackend: WoReportsExecutionRepository
+) extends RoReportsExecutionRepository with WoReportsExecutionRepository with CachedRepository {
+
+  /*
+   * We need to synchronise on cache to avoid the case:
+   * - initial state: RUNS_0 in backend
+   * - write RUNS_1 (cache => None) : ok, only write in backend
+   * [interrupt before actual write]
+   * - read: cache = None => update cache with RUNS_0
+   * - cache = RUNS_0
+   * [resume write]
+   * - write in backend RUNS_1
+   *
+   * => cache will never ses RUNS_1 before a clear.
+   */
+
+  /*
+   * The cache is managed node by node, i.e it can be initialized
+   * for certain and not for other.
+   * The initialization criteria (and so, the fact that the cache
+   * can be used for a given node) is given by the presence of the
+   * nodeid in map's keys.
+   */
+  private[this] var cache = Map[NodeId, Option[AgentRun]]()
+
+
+  override def clearCache(): Unit = this.synchronized {
+    cache = Map()
+  }
+
+  override def getNodesLastRun(nodeIds: Set[NodeId]): Box[Map[NodeId, Option[AgentRun]]] = this.synchronized {
+    for {
+      runs <- readBackend.getNodesLastRun(nodeIds.diff(cache.keySet))
+    } yield {
+      cache = cache ++ runs
+      cache.filterKeys { x => nodeIds.contains(x) }
+    }
+  }
+
+  override def updateExecutions(executions : Seq[AgentRun]) : Box[Seq[AgentRun]] = this.synchronized {
+    for {
+      runs <- writeBackend.updateExecutions(executions)
+    } yield {
+      cache = cache ++ runs.map(x => (x.agentRunId.nodeId, Some(x))).toMap
+      runs
+    }
+  }
 }
