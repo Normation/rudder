1bf661c12d00e5b09701ecf002093133ef38ccb4
Francois @fanf42 Armand <francois.armand@normation.com>
2015-01-30 14:33:13 +0100

    Fixes #6207: Deleting a directive is VERY slow

--- rudder-web/src/main/scala/com/normation/rudder/web/services/ReportDisplayer.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/services/ReportDisplayer.scala
@@ -148,9 +148,9 @@ class ReportDisplayer(
         </tr>
       </thead>
       <tbody>{ directives.flatMap { directive =>
-      directiveRepository.getDirective(directive.directiveId) match {
-        case Full(dir) =>
-          val tech = directiveRepository.getActiveTechnique(dir.id).map(act => techniqueRepository.getLastTechniqueByName(act.techniqueName).map(_.name).getOrElse("Unknown technique")).getOrElse("Unknown technique")
+      directiveRepository.getActiveTechniqueAndDirective(directive.directiveId) match {
+        case Full((activeTechnique, dir)) =>
+          val tech = techniqueRepository.getLastTechniqueByName(activeTechnique.techniqueName).map(_.name).getOrElse("Unknown technique")
           val techversion = dir.techniqueVersion;
           val tooltipid = Helpers.nextFuncName
           val severity = ReportType.getSeverityFromStatus(directive.directiveReportType)
@@ -659,8 +659,11 @@ class ReportDisplayer(
             val values = (component.componentValues++component.unexpectedCptValues).filter(value => value.cptValueReportType==UnknownReportType&&value.message.size==0)
             values.map(value => (component.component,value.componentValue))
             }
-          val tech = directiveRepository.getActiveTechnique(dir.directiveId).map(tech => techniqueRepository.getLastTechniqueByName(tech.techniqueName).map(_.name).getOrElse("Unknown Technique")).getOrElse("Unknown Technique")
-          val techVersion = directiveRepository.getDirective(dir.directiveId).map(_.techniqueVersion.toString).getOrElse("N/A")
+          val (tech, techVersion ) = directiveRepository.getActiveTechniqueAndDirective(dir.directiveId) match {
+            case Full((at, dir)) =>
+              (techniqueRepository.getLastTechniqueByName(at.techniqueName).map(_.name).getOrElse("Unknown Technique"), dir.techniqueVersion.toString)
+            case _ => ("Unknown Technique", "N/A")
+          }
           componentsReport.map(compo=> (compo,tech,techVersion))}
         techniqueComponentsReports }
 
@@ -754,8 +757,11 @@ class ReportDisplayer(
             val values = (component.componentValues++component.unexpectedCptValues).filter(value => value.cptValueReportType==UnknownReportType&&value.message.size!=0)
             values.map(value => (component.component,value.componentValue,value.message))
             }
-          val tech = directiveRepository.getActiveTechnique(dir.directiveId).map(tech => techniqueRepository.getLastTechniqueByName(tech.techniqueName).map(_.name).getOrElse("Unknown Technique")).getOrElse("Unknown Technique")
-          val techVersion = directiveRepository.getDirective(dir.directiveId).map(_.techniqueVersion.toString).getOrElse("N/A")
+          val (tech, techVersion ) = directiveRepository.getActiveTechniqueAndDirective(dir.directiveId) match {
+            case Full((at, dir)) =>
+              (techniqueRepository.getLastTechniqueByName(at.techniqueName).map(_.name).getOrElse("Unknown Technique"), dir.techniqueVersion.toString)
+            case _ => ("Unknown Technique", "N/A")
+          }
           componentsReport.map(compo=> (compo,tech,techVersion))}
         techniqueComponentsReports }
        if (reports.size >0){
