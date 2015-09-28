ef3774900dd1eb43b2703045e48bb52bcedf4252
Benoît Peccatte <benoit.peccatte@normation.com>
2015-09-28 11:47:58 +0200

    Fixes #7231: Errors and lots of warnings when tetsing ncf with python3

--- tests/unit/test_ncf_rudder.py
+++ tests/unit/test_ncf_rudder.py
@@ -6,55 +6,63 @@ import ncf_rudder
 import re
 import os.path
 import xml.etree.cElementTree as XML
+from pprint import pprint
 
 class TestNcfRudder(unittest.TestCase):
 
   def setUp(self):
+    self.maxDiff = None
     self.test_expected_reports_csv_file = os.path.realpath('test_expected_reports.csv')
-    self.test_expected_reports_csv_content = open(self.test_expected_reports_csv_file).read()
+    with open(self.test_expected_reports_csv_file) as fd:
+      self.test_expected_reports_csv_content = fd.read()
     
     self.test_technique_file = os.path.realpath('test_technique.cf')
-    self.technique_content = open(self.test_technique_file).read()
+    with open(self.test_technique_file) as fd:
+      self.technique_content = fd.read()
     self.technique_metadata = ncf.parse_technique_metadata(self.technique_content)
     method_calls = ncf.parse_technique_methods(self.test_technique_file)
     self.technique_metadata['method_calls'] = method_calls
 
     self.test_reporting = os.path.realpath('test_technique_reporting.cf')
-    self.reporting_content = open(self.test_reporting).read()
+    with open(self.test_reporting) as fd:
+      self.reporting_content = fd.read()
     self.reporting_metadata = ncf.parse_technique_metadata(self.reporting_content)
     reporting_method_calls = ncf.parse_technique_methods(self.test_reporting)
     self.reporting_metadata['method_calls'] = reporting_method_calls
 
     self.test_reporting_with_var = os.path.realpath('test_technique_with_variable.cf')
-    self.reporting_with_var_content = open(self.test_reporting_with_var).read()
+    with open(self.test_reporting_with_var) as fd:
+      self.reporting_with_var_content = fd.read()
     self.reporting_with_var_metadata = ncf.parse_technique_metadata(self.reporting_with_var_content)
     reporting_with_var_method_calls = ncf.parse_technique_methods(self.test_reporting_with_var)
     self.reporting_with_var_metadata['method_calls'] = reporting_with_var_method_calls
 
 
     self.test_any_technique = os.path.realpath('test_technique_any.cf')
-    self.any_technique_content = open(self.test_any_technique).read()
+    with open(self.test_any_technique) as fd:
+      self.any_technique_content = fd.read()
     self.any_technique_metadata = ncf.parse_technique_metadata(self.any_technique_content)
     any_technique_method_calls = ncf.parse_technique_methods(self.test_any_technique)
     self.any_technique_metadata['method_calls'] = any_technique_method_calls
 
     self.test_metadata_xml_file = os.path.realpath('test_metadata.xml')
-    self.test_metadata_xml_content = open(self.test_metadata_xml_file).read()
+    with open(self.test_metadata_xml_file) as fd:
+      self.test_metadata_xml_content = fd.read()
 
   def test_expected_reports_from_technique(self):
     expected_reports_string = ncf_rudder.get_technique_expected_reports(self.technique_metadata)
-    self.assertEquals(expected_reports_string, self.test_expected_reports_csv_content)
+    self.assertEqual(expected_reports_string, self.test_expected_reports_csv_content)
 
   def test_metadata_xml_from_technique(self):
     """The XML content generated from a ncf technique must match the XML content required by Rudder for it's metadata.xml file"""
     metadata_xml_string = ncf_rudder.get_technique_metadata_xml(self.technique_metadata)
     expected_metadata_pure_xml = self.test_metadata_xml_content
-    self.assertEquals(expected_metadata_pure_xml, metadata_xml_string)
+    self.assertEqual(expected_metadata_pure_xml, metadata_xml_string)
 
   def test_path_for_technique_dir(self):
     root_path = '/tmp/ncf_rudder_tests'
     path = ncf_rudder.get_path_for_technique(root_path, self.technique_metadata)
-    self.assertEquals(os.path.join(root_path, 'bla', '0.1'), path)
+    self.assertEqual(os.path.join(root_path, 'bla', '0.1'), path)
 
   def test_rudder_reporting_file(self):
     root_path = '/tmp/ncf_rudder_tests_reporting'
@@ -93,7 +101,7 @@ class TestNcfRudder(unittest.TestCase):
 
     generated_result = ncf_rudder.generate_rudder_reporting(self.reporting_metadata)
 
-    self.assertEquals(result, generated_result)
+    self.assertEqual(result, generated_result)
 
 
   def test_rudder_reporting_with_variable_content(self):
@@ -115,7 +123,7 @@ class TestNcfRudder(unittest.TestCase):
 
     generated_result = ncf_rudder.generate_rudder_reporting(self.reporting_with_var_metadata)
 
-    self.assertEquals(result, generated_result)
+    self.assertEqual(result, generated_result)
 
 
   def test_category_xml_content(self):
@@ -132,7 +140,7 @@ class TestNcfRudder(unittest.TestCase):
     result = '\n'.join(content)+"\n"
     generated_result = ncf_rudder.get_category_xml()
 
-    self.assertEquals(result, generated_result)
+    self.assertEqual(result, generated_result)
 
   # Other required functions:
   # ncf_rudder.write_technique_for_rudder(path, technique_metadata)
