2bb229819f50f29283821797412dd717dd1e2f6c
Nicolas Charles <nicolas.charles@normation.com>
2015-10-16 18:49:30 +0200

    Fixes #7286: Link from node/directive list to node detail is broken on Firefox (branch 2.10)

--- rudder-web/src/main/scala/com/normation/rudder/web/snippet/configuration/DirectiveManagement.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/snippet/configuration/DirectiveManagement.scala
@@ -154,7 +154,7 @@ class DirectiveManagement extends DispatchSnippet with Loggable {
     JsRaw("""
         var directiveId = null;
         try {
-          directiveId = JSON.parse(window.location.hash.substring(1)).directiveId ;
+          directiveId = JSON.parse(decodeURI(window.location.hash.substring(1))).directiveId ;
         } catch(e) {
           directiveId = null;
         }
@@ -216,7 +216,7 @@ class DirectiveManagement extends DispatchSnippet with Loggable {
     JsRaw(s"""
         var directiveId = null;
         try {
-          directiveId = JSON.parse(window.location.hash.substring(1)).directiveId ;
+          directiveId = JSON.parse(decodeURI(window.location.hash.substring(1))).directiveId ;
         } catch(e) {
           directiveId = null;
         }
