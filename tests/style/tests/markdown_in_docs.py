c54f350f9993c9fe541eab5beb416f85c77c8de0
Alexis Mousset <alexis.mousset@normation.com>
2016-04-28 17:03:16 +0200

    Fixes #8217: Broken syntax in generic methods docs

---
+++ tests/style/tests/markdown_in_docs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Test if the documentation fields contain unescaped dollar characters that would break pdflatex
+
+import ncf
+import re
+import sys
+
+from pprint import pprint
+
+if __name__ == '__main__':
+
+  # Get all generic methods
+  generic_methods = ncf.get_all_generic_methods_metadata()["data"]
+  test_file = sys.argv[0]
+  
+  check_backquotes = re.compile('[^\`]*\$[^\`]*')
+  errors = 0
+  
+  for name, method in generic_methods.iteritems():
+    if "documentation" in method:
+      if check_backquotes.match(method["documentation"]):
+        print("Test "+name+" has non escaped $ in its documentation")
+        errors += 1
+  
+  if errors == 0:
+    print("R: "+test_file+" Pass")
+  else:
+    print("R: "+test_file+" FAIL")
