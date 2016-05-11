cc15661fc1a8d4cf47f46df4aecd6c498d367bb2
Raphaël Gauthier <rga@normation.com>
2016-05-11 11:05:43 +0200

    Fixes #8262: Prevent red color usage in small graphs

--- rudder-web/src/main/webapp/javascript/rudder/homePage.js
+++ rudder-web/src/main/webapp/javascript/rudder/homePage.js
@@ -110,7 +110,8 @@ function homePage (
 }
 
 function displayInventoryGraph (id,data) {
-
+  var colorPatternDonutCharts = ['rgb(31, 119, 180)', 'rgb(23, 190, 207)', 'rgb(255, 127, 14)', 'rgb(255, 224, 14)', 'rgb(227, 119, 194)', 'rgb(44, 160, 44)', 'rgb(255, 104, 105)', 'rgb(148, 103, 189)', 'rgb(140, 86, 75)', 'rgb(160, 160, 160)', 'rgb(146, 201, 74)', '#ffd203', 'rgb(132, 63, 152)'];
+    
   var smallHeight =  $(window).height() / 4 ;
   
   // The chart, without legend
@@ -131,6 +132,9 @@ function displayInventoryGraph (id,data) {
       , legend :  {
           show : false
         }
+      , color: {
+        pattern: colorPatternDonutCharts
+      }
     });
 
   // Define a 'chart' which will only display its legend with interactin with the chart defind above
@@ -175,6 +179,9 @@ function displayInventoryGraph (id,data) {
            }
          }
        }
+      , color: {
+        pattern: colorPatternDonutCharts
+      }
     });
 
   // append charts
