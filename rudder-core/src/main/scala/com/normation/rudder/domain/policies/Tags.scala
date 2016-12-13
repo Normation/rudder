bc23eda4417621ae4964ed59d964b3399cb686ff
Nicolas Charles <nicolas.charles@normation.com>
2016-12-13 09:05:03 +0100

    Fixes #9734: Store tags on Directives/Rules

--- rudder-core/src/main/scala/com/normation/rudder/domain/policies/Tags.scala
+++ rudder-core/src/main/scala/com/normation/rudder/domain/policies/Tags.scala
@@ -1,6 +1,6 @@
 /*
 *************************************************************************************
-* Copyright 2011 Normation SAS
+* Copyright 2016 Normation SAS
 *************************************************************************************
 *
 * This file is part of Rudder.
@@ -34,43 +34,57 @@
 *
 *************************************************************************************
 */
-
 package com.normation.rudder.domain.policies
-import com.normation.utils.HashcodeCaching
-import com.normation.rudder.rule.category.RuleCategoryId
 
-case class RuleId(value:String) extends HashcodeCaching
-
-case class SerialedRuleId(ruleId : RuleId, serial : Int) extends HashcodeCaching
+import net.liftweb.common._
+import net.liftweb.util.Helpers.tryo
+import com.normation.utils.Control._
 
 /**
- * A rule is a binding between a set of directives
- * and some target (group of node, etc) on which applying
- * these directives.
- *
- * A rule may be stored in a pending state, for
- * example if it is not fully initialized.
- * In that case, it *MUST* be considered desactivated, whatever
- * the isEnabledField say.
+ * Tags that apply on Rules and Directives
+ * We do not warranty unicity of tags name, only of tagsname + tagvalue
  */
-case class Rule(
-    id              : RuleId
-  , name            : String
-  , serial          : Int
-  , categoryId      : RuleCategoryId
-    //is not mandatory, but if not present, rule is disabled
-  , targets         : Set[RuleTarget] = Set()
-    //is not mandatory, but if not present, rule is disabled
-  , directiveIds    : Set[DirectiveId] = Set()
-  , shortDescription: String = ""
-  , longDescription : String = ""
-  , isEnabledStatus : Boolean = false
-  , isSystem        : Boolean = false
-    /**
-     * Optionally, Rule can have Tags
-     */
-   , tags           : Option[Tags] = None
-) extends HashcodeCaching {
-  //system object must ALWAYS be ENABLED.
-  def isEnabled = isSystem || (isEnabledStatus && !targets.isEmpty && !directiveIds.isEmpty)
+final case class TagName( name : String )
+
+final case class TagValue( value : String )
+
+final case class Tag( tagName : TagName, tagValue : TagValue )
+
+/**
+ * We can have multiple Tags with same name - unicity is really on TagName + TagValue
+ */
+final case class Tags(
+  tags : Set[Tag] )
+
+final object JsonTagSerialisation {
+  import net.liftweb.json._
+  import net.liftweb.json.JsonDSL._
+
+  def serializeTags( tags : Tags ) : String = {
+
+    // sort all the tags by name
+    val m : JValue = JArray( tags.tags.toList.sortBy( _.tagName.name ).map {
+      case Tag( tagName, tagValue ) =>
+        ( tagName.name -> tagValue.value ) : JObject
+    } )
+
+    compactRender( m )
+  }
+
+  def unserializeTags(value:String): Box[Tags] = {
+    import net.liftweb.json.JsonParser._
+    implicit val formats = DefaultFormats
+
+    // This is awful
+    boxSequence(parse(value).children.map {
+      case JObject(fields) =>
+        fields.map{ case JField(tagName, tagValueJson) => tagValueJson match {
+          case JString(tagValue) => Full(Tag(TagName(tagName), TagValue(tagValue)))
+          case _ => Failure(s"Invalid type for tagValue ${tagValueJson}")
+        }
+      }
+      case _ => Failure(s"Invalid object in JSON serialization for Tags ${value}")  :: Nil
+    }.flatten).map( x =>  Tags(x.toSet))
+
+  }
 }
