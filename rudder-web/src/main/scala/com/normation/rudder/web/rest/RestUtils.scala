900afec9f707827f34357e9b6df2d47732c32eda
Vincent Membré <vincent.membre@normation.com>
2017-02-27 16:19:11 +0100

    Fixes #10295: Log an error when Rest API fails

--- rudder-web/src/main/scala/com/normation/rudder/web/rest/RestUtils.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/rest/RestUtils.scala
@@ -171,6 +171,12 @@ object RestUtils extends Loggable {
   }
 
   private[this] def effectiveResponse (id:Option[String], message:JValue, status:HttpStatus, action : String , prettify : Boolean) : LiftResponse = {
+    status match {
+      case _:RestError =>
+        // Log any error
+        logger.error(compactRender(message))
+      case _ => // Do nothing
+    }
     val printer: Document => String = if (prettify) Printer.pretty else Printer.compact
     val json = ( "action" -> action ) ~
                   ( "id"     -> id ) ~
