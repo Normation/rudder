a979617c3370258f088bbaa1dd974f7b61ff4aa3
Alexis Mousset <alexis.mousset@normation.com>
2017-08-17 11:24:33 +0200

    Fixes #11275: Parent fix breaks the tests

--- tests/unit/test_technique.ps1
+++ tests/unit/test_technique.ps1
@@ -14,7 +14,7 @@ function content_escaping_test {
 
   $class = "redhat"
   if (Evaluate-Class $class $local_classes $system_classes) {
-    $local_classes = Merge-ClassContext $local_classes $(File-Replace-Lines -File "/etc/httpd/conf/httpd.conf" -Line "ErrorLog \"/var/log/httpd/error_log\"" -Replacement "ErrorLog \"/projet/logs/httpd/error_log\"" -reportId $reportId -techniqueName $techniqueName -auditOnly:$auditOnly).get_item("classes")
+    $local_classes = Merge-ClassContext $local_classes $(File-Replace-Lines -File "/etc/httpd/conf/httpd.conf" -Line "ErrorLog `"/var/log/httpd/error_log`"" -Replacement "ErrorLog `"/projet/logs/httpd/error_log`"" -reportId $reportId -techniqueName $techniqueName -auditOnly:$auditOnly).get_item("classes")
   } else {
     _rudder_common_report_na -componentName "File replace lines" -componentKey "/etc/httpd/conf/httpd.conf" -message "Not applicable" -reportId $reportId -techniqueName $techniqueName -auditOnly:$auditOnly
   }
