5ef1ab4bf3da669ad7243c8318176e25f28eb745
Vincent Membré <vincent.membre@normation.com>
2017-11-24 11:36:13 +0100

    Fixes #11749: Add logic to generate ncf Technique files into Rudder

--- rudder-core/src/main/scala/com/normation/rudder/repository/json/JsonExctractorUtils.scala
+++ rudder-core/src/main/scala/com/normation/rudder/repository/json/JsonExctractorUtils.scala
@@ -81,19 +81,19 @@ trait JsonExctractorUtils[A[_]] {
     extractJson(json, key, jsonValueFun, { case obj : JObject => obj } ).map(x => monad.map(x)(identity))
   }
 
-  def extractJsonListString[T] (json: JValue, key: String)( convertTo: List[String] => Box[T] ): Box[Option[T]] = {
+  def extractJsonListString[T] (json: JValue, key: String)( convertTo: List[String] => Box[T] ): Box[A[T]] = {
     json \ key match {
       case JArray(values) =>
-        for {
+        (for {
           strings <- sequence(values) { _ match {
                         case JString(s) => Full(s)
                         case x => Failure(s"Error extracting a string from json: '${x}'")
                       } }
           converted <- convertTo(strings.toList)
         } yield {
-          Some(converted)
-        }
-      case JNothing   => Full(None)
+          converted
+        }).map(monad.point(_))
+      case JNothing   => convertTo(Nil).map(monad.point(_))
       case _              => Failure(s"Not a good value for parameter ${key}")
     }
   }
