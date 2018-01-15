acde38e22c70b9a9a723452fa395e1f854bc461c
Francois @fanf42 Armand <francois.armand@normation.com>
2018-01-15 15:08:40 +0100

    Fixes #11969: Remove \"non-uniry def overrides unary def\"

--- rudder-web/src/main/scala/com/normation/rudder/web/components/popup/ModificationValidationPopup.scala
+++ rudder-web/src/main/scala/com/normation/rudder/web/components/popup/ModificationValidationPopup.scala
@@ -383,7 +383,7 @@ class ModificationValidationPopup(
     new WBTextAreaField("Change audit message", "") {
       override def setFilter = notNull _ :: trim _ :: Nil
       override def inputField = super.inputField  %  ("style" -> "height:8em;") % ("tabindex" -> "2") % ("placeholder" -> {userPropertyService.reasonsFieldExplanation})
-      override def validations() = {
+      override def validations = {
         if(mandatory){
           valMinLen(5, "The reason must have at least 5 characters.") _ :: Nil
         } else {
