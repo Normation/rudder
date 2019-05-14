46b589535524aa821fe978ccb744786f3173f880
Benoît Peccatte <benoit.peccatte@normation.com>
2019-05-14 18:42:25 +0200

    Fixes #14886: Move away from python 2 to python 3 in relay-api

--- relay/sources/relay-api/run.py
+++ relay/sources/relay-api/run.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!flask/bin/python3
 
 # This file is only present for development/ local test and should not be used in production
 # To deploy ncf api you should use it with a virtual environment and a wsgi file
