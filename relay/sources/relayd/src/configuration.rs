76b1a45e1d4118762171570db835b2e4de4b0504
Alexis Mousset <alexis.mousset@rudder.io>
2019-07-19 15:13:55 +0000

    Update configuration.rs

--- relay/sources/relayd/src/configuration.rs
+++ relay/sources/relayd/src/configuration.rs
@@ -27,7 +27,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Rudder.  If not, see <http://www.gnu.org/licenses/>.
-
+broken
 use crate::{data::node::NodeId, error::Error};
 use itertools::Itertools;
 use serde::Deserialize;
