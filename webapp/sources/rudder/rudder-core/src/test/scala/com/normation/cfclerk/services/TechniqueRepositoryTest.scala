91fa73f971df561e0af5cae85724820f05a8d549
Francois @fanf42 Armand <francois.armand@normation.com>
2019-09-10 12:20:22 +0200

    Fixes #15673: Parent ticket didn't corrected the problem when a category is moved to root

--- webapp/sources/rudder/rudder-core/src/test/scala/com/normation/cfclerk/services/TechniqueRepositoryTest.scala
+++ webapp/sources/rudder/rudder-core/src/test/scala/com/normation/cfclerk/services/TechniqueRepositoryTest.scala
@@ -268,7 +268,7 @@ class TechniqueRepositoryTest extends Specification with Loggable with AfterAll 
     (testCallback.categories must containTheSameElementsAs(Seq(TechniqueCategoryModType.Moved(cat.id, cat2.id)))) and
     (fsRepos.getAllCategories.get(cat2.id) must beSome[TechniqueCategory]) and
     (ldapRepo.moved must beEqualTo(("fileSecurity", "Active Techniques", None) :: Nil) ) and
-    (ldapRepo.updatedTechniques must containTheSameElementsAs(Seq()) )
+    (ldapRepo.updatedTechniques must containTheSameElementsAs(Seq("fileTemplate", "copyGitFile")) )
 
   }
 }
