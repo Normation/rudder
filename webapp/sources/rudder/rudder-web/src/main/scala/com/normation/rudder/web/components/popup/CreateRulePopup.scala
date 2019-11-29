bf0492b59cbaa48265572196ede21c714e9fea49
Vincent Membré <vincent.membre@normation.com>
2019-11-29 10:07:04 +0100

    Fixes #16292: When creating a rule, we reach compliance tab not setting one

--- webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/components/popup/CreateRulePopup.scala
+++ webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/components/popup/CreateRulePopup.scala
@@ -217,7 +217,7 @@ class CreateOrCloneRulePopup(
             onFailure & onFailureCallback()
       }
       createRule & JsRaw( s"""
-        localStorage.setItem('Active_Rule_Tab', 0);
+        localStorage.setItem('Active_Rule_Tab', 1);
         var scope = angular.element( $$("#showFiltersRules")).scope();
         scope.$$apply(function(){
           scope.filterGlobal(scope.searchStr);
