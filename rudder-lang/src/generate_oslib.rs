8431161c637fdaf2c1722b3fd5a4e8a1bcb6bb65
gpoblon <gpoblon@student.42.fr>
2020-02-12 18:30:02 +0100

    Fixes #16717: rl generate enum mappings from os tree

--- rudder-lang/src/generate_oslib.rs
+++ rudder-lang/src/generate_oslib.rs
@@ -7,14 +7,28 @@ use ron::de::from_reader;
 use serde::Deserialize;
 use std::fs;
 
-
-#[derive(Debug, Deserialize)]
-struct OsTreeBuilder(Vec<OsTree>);
+#[derive(Default, Debug, Deserialize)]
+struct OsTreeBuilder {
+    types: Vec<String>,
+    families: Vec<String>,
+    major: Vec<String>,
+    minor: Vec<String>,
+    minor_map: Vec<String>,
+}
 
 #[derive(Debug, Deserialize)]
 enum OsTree {
     Oses(Vec<Os>),
     Family((String, Vec<Box<OsTree>>)),
+    Type((String, Vec<Box<OsTree>>)),
+}
+
+// Mapping purpose only, allows to print an os in the right container which is either a type or a family
+#[derive(Debug, Clone)]
+enum ParentBranch {
+    Family(String),
+    Type(String),
+    None,
 }
 
 /// A Version is composed of a `major` and an array of `minor`s
@@ -28,59 +42,114 @@ struct Os(String, Vec<Version>);
 /// Generates an Os and all its possible variants as a vec of stringified Os'es
 impl Os {
     fn get_name(&self) -> &str { &self.0 }
-    fn get_versions(&self) -> &Vec<Version> { &self.1 }
-
-    fn as_vec(&self) -> Vec<String> {
-        let osname = self.get_name();
-        let mut osfull: Vec<String> = Vec::new();
-        // add a comment line to visually separate oses
-        // osfull.push(format!("# {}", osname));
-        osfull.push(osname.to_owned());
-        for version in self.get_versions() {
-            if let Some(major) = &version.0 {
-                if let Some(minors) = &version.1 {
-                    for minor in minors {
-                        osfull.push(format!("{}_{}_{}", osname, major, minor));
-                    }
-                } else {
-                    osfull.push(format!("{}_{}", osname, major));
-                }
-            }
+    fn get_versions(&self) -> Vec<Version> {
+        // hack to make sure enven with an empty array a default major is created
+        if self.1.len() > 0 {
+            self.1.clone()
+        } else {
+            vec![(None, None)]
         }
-        osfull
+    }
+
+    fn as_vec(&self, tree_builder: &mut OsTreeBuilder, parent: &ParentBranch) {
+        let osname = self.get_name();
+
+        match parent {
+            ParentBranch::Type(name) => tree_builder.types.push(format!("{} -> {}", osname, name)),
+            ParentBranch::Family(name) => tree_builder.families.push(format!("{} -> {}", osname, name)),
+            _ => ()
+        };
+
+        for version in &self.get_versions() {
+            let major = match &version.0 {
+                Some(m) => m.to_owned(),
+                None => "0".to_owned(),
+            };
+            let minors = match &version.1 {
+                Some(m) => m.to_vec(),
+                None => vec!["0".to_owned()],
+            };
+            for minor in minors {
+                tree_builder.minor.push(format!("{}_{}_{}", osname, major, minor));
+                tree_builder.minor_map.push(format!("{}_{}_{} -> {}_{}", osname, major, minor, osname, major));
+            }
+            tree_builder.major.push(format!("{}_{} -> {}", osname, major, osname));
+        }
     }
 }
 
+// fn aux_mapping_builder(mut mapped: &mut Vec<String>, mapper: &str) {}
+
 /// Browses an Os branch to convert it to a full list of supported oses
-fn rec_oslib_builder(os_tree: &OsTree, mut oslist: &mut Vec<String>) {
+fn rec_oslib_builder(os_tree: &OsTree, tree_builder: &mut OsTreeBuilder, parent: ParentBranch) {
     match os_tree {
         OsTree::Oses(oses) => {
             for os in oses {
-                oslist.extend(os.as_vec());
+                os.as_vec(tree_builder, &parent);
             }
         },
         OsTree::Family((name, osboxes)) => {
-            oslist.push(format!("{}, # family", name));
+            if let ParentBranch::Type(parent_name) = parent {
+                tree_builder.types.push(format!("{} -> {}", name, parent_name));
+            }
             for osbox in osboxes {
-                rec_oslib_builder(&*osbox, &mut oslist);
+                rec_oslib_builder(&*osbox, tree_builder, ParentBranch::Family(name.to_string()));
             }
         },
-    }
+        OsTree::Type((name, osboxes)) => {
+            for osbox in osboxes {
+                rec_oslib_builder(&*osbox, tree_builder, ParentBranch::Type(name.to_string()));
+            }
+        },
+    };
 }
 
 /// Reads the os builder .RON file and converts its content
 /// into a necessary rudder-lang `global enum os` that holds every single supported Os(kind)
 pub fn generate_oslib(fname: &str, oslib_filename: &str) -> Result<()> {
     info!("{} from {}", "Generating OS list".bright_green(), fname.bright_yellow());
-    let ftree = fs::File::open(fname).map_err(|e| err!(Token::new(&fname.to_owned(), ""), "{}", e))?;
-    let os_tree_builder: OsTreeBuilder = from_reader(ftree).map_err(|e| err!(Token::new(fname, ""), "{}", e))?;
+    let file_tree = fs::File::open(fname).map_err(|e| err!(Token::new(&fname.to_owned(), ""), "{}", e))?;
+    let recvd_tree: Vec<OsTree> = from_reader(file_tree).map_err(|e| err!(Token::new(fname, ""), "{}", e))?;
 
-    let mut oslist: Vec<String> = Vec::new();
-    os_tree_builder.0.iter().for_each(|os_tree| rec_oslib_builder(os_tree, &mut oslist));
+    let mut tree_builder = OsTreeBuilder::default();
+    recvd_tree.iter().for_each(|branch| rec_oslib_builder(branch, &mut tree_builder, ParentBranch::None));
     
     // // write global enum os in a file
-    let content = format!("@format=0\n\nglobal enum os {{\n  {}\n}}", oslist.join(",\n  "));
-    {    
+    let content = format!(
+r#"@format=0
+
+enum os ~> family {{
+  {},
+  *->*
+}}
+
+enum family ~> type {{
+  {},
+  *->*
+}}
+
+enum major ~> os {{
+  {},
+  *->*
+}}
+
+enum minor ~> major {{
+  {},
+  *->*
+}}
+
+global enum minor {{
+  {}
+}}
+"#,
+        tree_builder.families.join(",\n  "),
+        tree_builder.types.join(",\n  "),
+        tree_builder.major.join(",\n  "),
+        tree_builder.minor_map.join(",\n  "),
+        tree_builder.minor.join(",\n  "),
+    );
+    {
+        // has to create a scope bc write was sometimes not working properly
         fs::write(oslib_filename, content.as_bytes()).expect("Could not write content to os lib file");
     }
     Ok(())
