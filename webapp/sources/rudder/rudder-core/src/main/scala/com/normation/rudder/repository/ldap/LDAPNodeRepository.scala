009e5da8121819108b14626a4e4fdafcd082148b
Francois @fanf42 Armand <francois.armand@normation.com>
2020-03-18 17:29:58 +0100

    Revert "Fixes #16906: Be able to save in LDAP a Policy Server Target"
    My bad - I checked `accept` in place of comment :/
    
    This reverts commit b0bfc618e3b631faa8bade7909a1c15b021b03c1.

--- webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/repository/ldap/LDAPNodeRepository.scala
+++ webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/repository/ldap/LDAPNodeRepository.scala
@@ -54,7 +54,6 @@ import com.normation.inventory.domain.KeyStatus
 import com.normation.inventory.domain.NodeId
 import com.normation.inventory.domain.SecurityToken
 import com.normation.inventory.ldap.core.InventoryDit
-import com.normation.rudder.domain.RudderDit
 import zio._
 import zio.syntax._
 
@@ -64,7 +63,6 @@ class WoLDAPNodeRepository(
   , mapper              : LDAPEntityMapper
   , ldap                : LDAPConnectionProvider[RwLDAPConnection]
   , actionLogger        : EventLogRepository
-  , rudderDit           : RudderDit
 ) extends WoNodeRepository with NamedZioLogger {
   repo =>
 
@@ -185,27 +183,5 @@ class WoLDAPNodeRepository(
     }
 
     ZIO.when(newNode.id == Constants.ROOT_POLICY_SERVER_ID) { validateRoot(newNode) }
-  }
-
-  override def createNode(node: Node, modId: ModificationId, actor: EventActor, reason: Option[String]): IOResult[Node] = {
-    val entry = mapper.nodeToEntry(node)
-    for {
-      con <- ldap
-      res <- con.save(entry).chainError(s"Error when trying to save node in promote to relay ${entry.dn}")
-      _ <- IOResult.effect(println(s"-----  ENTRY  : $entry"))
-      _ <- IOResult.effect(println(s"-----  RESULT : $res"))
-    } yield {
-      node
-    }
-  }
-
-  override def deleteNode(node: Node, modId: ModificationId, actor: EventActor, reason: Option[String]): IOResult[Node] = {
-    val entry = rudderDit.NODE_CONFIGS.dn
-    for {
-      con <- ldap
-      res <- con.delete(entry).chainError(s"Error when trying to delete node '${node.id.value}")
-    } yield {
-      node
-    }
   }
 }
