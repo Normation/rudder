662e26471574107554ccd0f7c16a00d36d096785
Benoît Peccatte <benoit.peccatte@normation.com>
2020-04-02 16:52:50 +0200

    Fixes #17010: Cleanup rudder-lang tester

--- rudder-lang/tools/tester.sh
+++ rudder-lang/tools/tester.sh
@@ -1,28 +1,73 @@
 #!/bin/bash
 
-set -xe # -x # to print every line as it occurs
+set -e
+set -x # -x # to print every line as it occurs
 
-dir=/tmp/rudderc/tester/
-cfjson_tester=/opt/rudder/share/rudder-lang/tools/cfjson_tester
-technique_path=/var/rudder/configuration-repository/techniques/ncf_techniques/$1/1.0/technique.cf
+if [ "$1" = "-k" ]
+then
+  cleanup=no
+  shift
+fi
 
-mkdir -p $dir/
+if [ "$1" = "" ]
+then
+  echo "Usage tester.sh [-k] <technique>"
+  echo " -k: keep temporary files after cleanup"
+  echo " <technique>: can be either a technique name from ncf directory or an absolute path"
+  exit 1
+fi
+
+technique="$1"
+
+# Default values for rudder server
+self_dir=$(dirname $(readlink -e $0))
+test_dir=$(mktemp -d)
+cfjson_tester="${self_dir}/cfjson_tester"
+
+# Detect technique path
+technique_path="/var/rudder/configuration-repository/techniques/ncf_techniques/${technique}/1.0/technique.cf"
+[ -f "${technique_path}" ] || technique_path="$1"
+if [ ! -f "${technique_path}" ]
+then
+  echo "Cannot find /var/rudder/configuration-repository/techniques/ncf_techniques/${technique}/1.0/technique.cf nor $1"
+  exit 1
+fi
+
+# Detect rudderc configuration
+config_file="/opt/rudder/etc/rudderc.conf"
+[ -f "${config_file}" ] || config_file="${self_dir}/rudderc.conf"
+if [ ! -f "${config_file}" ]
+then
+  echo "Cannot find /opt/rudder/etc/rudderc.conf nor ${self_dir}/rudderc.conf"
+  exit 1
+fi
+
+# Detect rudderc
+rudderc="/opt/rudder/bin/rudderc"
+[ -f "${rudderc}" ] || rudderc="cargo run -- "
 
 # Take original technique an make a json
-$cfjson_tester ncf-to-json $technique_path $dir/technique.json
+${cfjson_tester} ncf-to-json --config="${config_file}" "${technique_path}" "${test_dir}/technique.json"
 
 # Take json and produce a rudder-lang technique
-cargo run -- --translate -i $dir/technique.json
+${rudderc} --config-file "${config_file}" --translate -i "${test_dir}/technique.json"
 
 # Take rudder lang technique and compile it into cf file
 # output format is generated behind the scenes, it actually is a cf file, not rl
-cargo run -- -i $dir/technique.rl
+${rudderc} --config-file "${config_file}" -i "${test_dir}/technique.rl"
 
 # take generated cf file a new json
-$cfjson_tester ncf-to-json $dir/technique.rl.cf $dir/technique.rl.cf.json
+${cfjson_tester} ncf-to-json --config="${config_file}" "${test_dir}/technique.rl.cf" "${test_dir}/technique.rl.cf.json"
 
-# TODO compare generated json
-$cfjson_tester compare-json $dir/technique.json $dir/technique.rl.cf.json
+# compare generated json
+${cfjson_tester} compare-json --config="${config_file}" "${test_dir}/technique.json" "${test_dir}/technique.rl.cf.json"
 
-# TODO compare generated cf files
-$cfjson_tester compare-cf $technique_path $dir/technique.rl.cf
+# compare generated cf files
+${cfjson_tester} compare-cf --config="${config_file}" "${technique_path}" "${test_dir}/technique.rl.cf"
+
+if [ "${cleanup}" != "no" ]
+then
+  rm -rf "${test_dir}"
+else
+  echo "Done testing in ${test_dir}"
+fi
