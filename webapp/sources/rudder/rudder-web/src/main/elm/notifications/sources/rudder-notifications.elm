204a64dc2da61720c8155d529942831f4f188033
Vincent Membré <vincent.membre@normation.com>
2020-05-12 16:11:27 +0200

    Fixes #17383: Improve notification display in Rudder

--- webapp/sources/rudder/rudder-web/src/main/elm/notifications/sources/rudder-notifications.elm
+++ webapp/sources/rudder/rudder-web/src/main/elm/notifications/sources/rudder-notifications.elm
@@ -1,31 +1,28 @@
 port module RudderNotifications exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing ( style, class, type_, checked )
-import Html.Events exposing (..)
+import Html.Attributes exposing (  class, style )
 import String
 import Toasty
-import Toasty.Defaults
-import Http exposing (Error)
-import Http exposing (..)
-import Json.Encode as E
 import Browser
 
 
 port successNotification : (String -> msg) -> Sub msg
 port errorNotification   : (String -> msg) -> Sub msg
 port warningNotification : (String -> msg) -> Sub msg
+port infoNotification    : (String -> msg) -> Sub msg
 
 ------------------------------
 -- SUBSCRIPTIONS
 ------------------------------
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
   Sub.batch
     [ successNotification CreateSuccessNotification
     , errorNotification   CreateErrorNotification
     , warningNotification CreateWarningNotification
+    , infoNotification CreateInfoNotification
     ]
 
 ------------------------------
@@ -54,15 +51,18 @@ main = Browser.element
 type alias Model =
   { contextPath  : String
   , message      : Maybe String
-  , toasties     : Toasty.Stack Toasty.Defaults.Toast
+  , toasties     : Toasty.Stack MyToast
   }
 
 type Msg
-  = ToastyMsg (Toasty.Msg Toasty.Defaults.Toast)
+  = ToastyMsg (Toasty.Msg MyToast)
   | CreateSuccessNotification String
   | CreateErrorNotification   String
   | CreateWarningNotification String
+  | CreateInfoNotification    String
 
+type MyToast =
+  Success String | Warning String | Error String | Info String
 
 ------------------------------
 -- UPDATE --
@@ -73,18 +73,18 @@ update msg model =
   case msg of
     ToastyMsg m ->
       Toasty.update defaultConfig ToastyMsg m model
-
     CreateSuccessNotification m ->
       (model, Cmd.none)
         |> (createSuccessNotification m)
-
     CreateErrorNotification m ->
       (model, Cmd.none)
         |> (createErrorNotification m)
-
     CreateWarningNotification m ->
       (model, Cmd.none)
         |> (createWarningNotification m)
+    CreateInfoNotification m ->
+      (model, Cmd.none)
+        |> (createInfoNotification m)
 ------------------------------
 -- VIEW --
 ------------------------------
@@ -92,7 +92,7 @@ update msg model =
 view: Model -> Html Msg
 view model =
   div [class "row"]
-    [ div[class "toasties"][Toasty.view defaultConfig Toasty.Defaults.view ToastyMsg model.toasties]
+    [ div[class "toasties"][Toasty.view defaultConfig viewToast ToastyMsg model.toasties]
     ]
 
 
@@ -101,31 +101,60 @@ view model =
 ------------------------------
 defaultConfig : Toasty.Config Msg
 defaultConfig =
-  Toasty.Defaults.config
+  Toasty.config
     |> Toasty.delay 9999999
-    |> Toasty.containerAttrs
-    [ class "rudder-notification"
-    ]
+    |> Toasty.transitionOutDuration 700
+    |> Toasty.transitionOutAttrs [ class "animated fadeOutRightBig", style "max-height" "0", style "margin-top" "0" ]
+    |> Toasty.transitionInAttrs [ class "animated bounceInRight" ]
+    |> Toasty.containerAttrs  [ class "rudder-notification" ]
 
 tempConfig : Toasty.Config Msg
 tempConfig = defaultConfig |> Toasty.delay 3000
 
-addTempToast : Toasty.Defaults.Toast -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
+addTempToast : MyToast -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 addTempToast toast ( model, cmd ) =
   Toasty.addToast tempConfig ToastyMsg toast ( model, cmd )
 
-addToast : Toasty.Defaults.Toast -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
+addToast : MyToast -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 addToast toast ( model, cmd ) =
   Toasty.addToast defaultConfig ToastyMsg toast ( model, cmd )
 
 createSuccessNotification : String -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 createSuccessNotification message =
-  addTempToast (Toasty.Defaults.Success "Success!" message)
+  addTempToast (Success  message)
 
 createWarningNotification : String -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 createWarningNotification message =
-  addToast (Toasty.Defaults.Warning "Warning:" message)
+  addToast (Warning message)
 
 createErrorNotification   : String -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 createErrorNotification   message =
-  addToast (Toasty.Defaults.Error "Error..." message)
+  addToast (Error  message)
+
+createInfoNotification   : String -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
+createInfoNotification   message =
+  addTempToast (Info  message)
+
+
+viewToast : MyToast -> Html msg
+viewToast toast =
+    case toast of
+        Success message ->
+            genericToast "callout-success" "fa-check-circle" "Success" message
+        Warning message ->
+            genericToast "callout-warning" "fa-exclamation-circle" "Warning" message
+        Error message ->
+            genericToast "callout-danger" "fa-times-circle" "Error" message
+        Info message ->
+            genericToast "callout-info" "fa-info-circle" "Info" message
+
+
+genericToast : String -> String -> String -> String -> Html msg
+genericToast variantClass iconClass title message =
+    div
+        [ class "callout-fade", class variantClass ]
+        [
+         div [ class "marker"] [ span [ class "fa", class iconClass ] [] ]
+        , h4 [ ] [   text title ]
+        , p [ ] [ text message ]
+        ]
