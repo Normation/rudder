417ea8c8b3a52021b7ad2d13c37e8c0f81b586e0
Vincent Membré <vincent.membre@normation.com>
2020-06-30 21:55:28 +0200

    Fixes #17888: Optimize elm application

--- webapp/sources/rudder/rudder-web/src/main/elm/build-app.sh
+++ webapp/sources/rudder/rudder-web/src/main/elm/build-app.sh
@@ -6,6 +6,6 @@ ELM_DIR="$( cd "$( dirname "$0" )" && pwd )"
 PROJECTS=("notifications")
 for PROJECT in ${PROJECTS}; do
   cd ${ELM_DIR}/${PROJECT}
-  elm make sources/rudder-${PROJECT}.elm --output=generated/rudder-${PROJECT}.js
+  elm make --optimize sources/rudder-${PROJECT}.elm --output=generated/rudder-${PROJECT}.js
   cp generated/rudder-${PROJECT}.js ${ELM_DIR}/../webapp/javascript/rudder/elm/
 done
