4ae97dfcf3f847701cebaafe00aa8d9927359de2
Vincent Membré <vincent.membre@normation.com>
2021-07-22 19:04:38 +0200

    fixup!  Reorganise Datatypes

--- webapp/sources/rudder/rudder-web/src/main/elm/rules/sources/JsonEncoder.elm
+++ webapp/sources/rudder/rudder-web/src/main/elm/rules/sources/JsonEncoder.elm
@@ -3,28 +3,28 @@ module  JsonEncoder exposing (..)
 import DataTypes exposing (..)
 import Json.Encode exposing (..)
 
-encodeRuleDetails: RuleDetails -> Value
+encodeRuleDetails: Rule -> Value
 encodeRuleDetails ruleDetails =
   let
     listTags = object(List.map (\t -> (t.key, string t.value)) ruleDetails.tags)
   in
       object [
-        ("id"               , string ruleDetails.id               )
-      , ("displayName"      , string ruleDetails.displayName      )
+        ("id"               , string ruleDetails.id.value               )
+      , ("displayName"      , string ruleDetails.name      )
       , ("category"         , string ruleDetails.categoryId       )
       , ("shortDescription" , string ruleDetails.shortDescription )
       , ("longDescription"  , string ruleDetails.longDescription  )
       , ("enabled"          , bool   ruleDetails.enabled          )
       , ("system"           , bool   ruleDetails.isSystem         )
-      , ("directves"        , list string ruleDetails.directives  )
-      , ("targets"          , list encodeTargets [ruleDetails.targets]   )
+      , ("directves"        , list string (List.map .value ruleDetails.directives)  )
+      , ("targets"          , list encodeTargets ruleDetails.targets   )
       , ("tags"             , list encodeTags ruleDetails.tags    )
       ]
 
 encodeDirectives: Directive -> Value
 encodeDirectives directive=
   object [
-    ("id"               , string directive.id)
+    ("id"               , string directive.id.value)
   , ("displayName"      , string directive.displayName)
   , ("longDescription"  , string directive.longDescription)
   , ("techniqueName"    , string directive.techniqueName)
@@ -34,25 +34,28 @@ encodeDirectives directive=
   , ("policyMode"       , string directive.policyMode)
   ]
 
-encodeTargets : Targets -> Value
-encodeTargets targets =
-  object [
-    ("include", encodeTargetsIncludeExclude targets.include)
-  , ("exclude", encodeTargetsIncludeExclude targets.exclude)
-  ]
+encodeTargets : RuleTarget -> Value
+encodeTargets target =
+  case target of
+    NodeGroupId id -> string ("group:" ++ id)
+    Special spe -> string spe
+    Node id -> string ("node:"++id)
+    Composition include exclude ->
+      object [
+        ("include", encodeTargets include)
+      , ("exclude", encodeTargets exclude)
+      ]
+    Or t ->
+      object [
+        ("or", list encodeTargets t)
+      ]
+    And t ->
+      object [
+        ("and", list encodeTargets t)
+      ]
 
 encodeTags : Tag -> Value
 encodeTags tag =
   object [
     (tag.key, string tag.value)
   ]
-
-encodeTargetsIncludeExclude : List String -> Value
-encodeTargetsIncludeExclude targets =
-  let
-    tt = List.map (\t -> "group:"++t) targets
-  in
-  object [
-    ("or", list string tt)
-  ]
-
