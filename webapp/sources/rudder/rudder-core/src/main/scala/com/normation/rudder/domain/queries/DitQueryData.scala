f6cc4822ec10b04b95429438d1449eced9a0c977
Francois @fanf42 Armand <francois.armand@normation.com>
2021-07-26 10:49:44 +0200

    Fixes #19559: Can't access filesystem type from group criteria

--- webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/domain/queries/DitQueryData.scala
+++ webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/domain/queries/DitQueryData.scala
@@ -230,6 +230,7 @@ class DitQueryData(dit: InventoryDit, nodeDit: NodeDit, rudderDit: RudderDit, ge
       Criterion(A_NETIF_TYPE_MIB, StringComparator)
     )),
     ObjectCriterion(OC_FS, leObjectCriterion.criteria ++ Seq(
+      Criterion(A_NAME, StringComparator),
       Criterion(A_MOUNT_POINT, StringComparator),
       Criterion(A_FILE_COUNT, StringComparator),
       Criterion(A_FREE_SPACE, MemoryComparator),
