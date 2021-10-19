cdbe38452c166f871b845e9f5caffb2cf0460c83
Alexis Mousset <alexis.mousset@rudder.io>
2021-10-19 17:27:26 +0200

    Fixes #20161: Add an exception to relayd license to allow linking statically with openssl

--- relay/sources/relayd/tests/shared_files.rs
+++ relay/sources/relayd/tests/shared_files.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later WITH GPL-3.0-linking-source-exception
 // SPDX-FileCopyrightText: 2019-2020 Normation SAS
 
 use rudder_relayd::{
