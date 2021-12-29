136f64cfd4a2d131082253041f99144bd39d8e47
ElaadF <elaad.f@gmail.com>
2021-12-29 01:38:35 +0100

    Fixes #20487: Cancel button is missing when deleting a technique

--- webapp/sources/rudder/rudder-web/src/main/elm/editor/sources/ViewTechnique.elm
+++ webapp/sources/rudder/rudder-web/src/main/elm/editor/sources/ViewTechnique.elm
@@ -282,7 +282,7 @@ view model =
                   text "Are you sure you want to Delete Technique 'ressource test'?"
                 ]
               , div [ class "modal-footer" ] [
-                  button [ class "btn btn-primary btn-outline pull-left", onClick (ClosePopup Ignore) ] [ --ng-click="cancel()"></button>
+                  button [ class "btn btn-primary", onClick (ClosePopup Ignore) ] [ --ng-click="cancel()"></button>
                     text "Cancel "
                   , i [ class "fa fa-arrow-left" ] []
                   ]
