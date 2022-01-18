4e91afe4baaaadab72309c7d7f208c6394e60777
Raphael Gauthier <raphael.gauthier@normation.com>
2022-01-18 16:44:55 +0100

    Fixes #20581: Redesign the login page with the new graphic charter

--- webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/snippet/Login.scala
+++ webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/snippet/Login.scala
@@ -12,40 +12,45 @@ class Login extends DispatchSnippet with DefaultExtendableSnippet[Login] {
   def mainDispatch = Map(
     "display" -> { authForm:NodeSeq =>
       if(userListProvider.authConfig.users.isEmpty) {
-        <div class="col-xs-12" style="min-height:250px">
-          <div class="row" style="margin-top:30px">
-            <h3 class="text-center">
-              <i class="fa fa-check-circle" style="color: #9bc832;" aria-hidden="true"></i>
-              Rudder installation complete!
-            </h3>
-            <div class="text-center" style="margin-top:20px">
-              <i class="fa fa-info-circle" aria-hidden="true" style="color: #3694d1;"></i>
-              To get started, create a first user with:
+        <div>
+          <div class="logo-container">
+            <img src="/images/logo-rudder.svg" data-lift="with-cached-resource" alt="Rudder"/>
+          </div>
+          <div class="plugin-info"></div>
+          <form id="login-form">
+            <div class="motd"></div>
+            <div>
+              <div class="success-info">
+                <i class="fa fa-check"></i>
+                Rudder installation complete!
+              </div>
+              <p>
+                To get started, create a first user with:
+              </p>
+              <div class="group-cmd">
+                <span id="cmd-user" class="cmd-text">
+                  rudder server create-user -u &lt;username&gt;
+                </span>
+                <button class="btn btn-cmd-user btn-clipboard" type="button" data-clipboard-text="rudder server create-user -u " data-toggle='tooltip' data-placement='bottom' data-container="html" title="Copy to clipboard">
+                  <i class="far fa-clipboard"></i>
+                </button>
+              </div>
+              <script type="text/javascript">
+                // <![CDATA[
+                window.setTimeout('location.reload()', 10000);
+                new ClipboardJS('.btn-clipboard');
+                var checked;
+                $('.btn-cmd-user').on('click', function(){
+                  clearInterval(checked);
+                  $('.btn-cmd-user .fa-clipboard').attr('class', 'fas fa-check') ;
+                    checked = setInterval(function(){
+                    $('.btn-cmd-user .fa-check').attr('class', 'far fa-clipboard') ;
+                  },700);
+                });
+                // ]]>
+              </script>
             </div>
-          </div>
-          <div class="row" style="margin-top:10px">
-            <span class="cmd-server text-center col-xs-8 col-xs-offset-2">
-              <span id="cmd-user" class="cmd-text">
-                rudder server create-user -u &lt;username&gt;
-              </span>
-            </span>
-            <button class="btn btn-xs btn-cmd-user btn-clipboard" data-clipboard-text="rudder server create-user -u " data-toggle='tooltip' data-placement='bottom' data-container="html" title="Copy to clipboard">
-              <i class="far fa-clipboard"></i>
-            </button>
-          </div>
-          <script type="text/javascript">
-            // <![CDATA[
-            window.setTimeout('location.reload()', 10000);
-            new ClipboardJS('.btn-clipboard');
-            var btn = document.getElementsByClassName('btn-cmd-user');
-            btn[0].onclick = function(){
-              document.getElementsByClassName("fa-clipboard")[0].setAttribute("class", "fas fa-check") ;
-              var checked = setInterval(function(){
-                document.getElementsByClassName("fa-check")[0].setAttribute("class", "far fa-clipboard") ;
-               },3000);
-            }
-            // ]]>
-          </script>
+          </form>
         </div>
       } else authForm
     }
