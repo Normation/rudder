824bde890e3254f7b9408567c9b68b0c58dfdf4d
Nicolas Charles <nicolas.charles@normation.com>
2022-08-08 17:41:02 +0200

    Fixes #21571: Create a migration script for campaign

---
+++ webapp/sources/rudder/rudder-core/src/main/resources/Migration/dbMigration-7.1-7.2-create-campaign-table.sql
@@ -0,0 +1,50 @@
+/*
+*************************************************************************************
+* Copyright 2022 Normation SAS
+*************************************************************************************
+*
+* This file is part of Rudder.
+*
+* Rudder is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* In accordance with the terms of section 7 (7. Additional Terms.) of
+* the GNU General Public License version 3, the copyright holders add
+* the following Additional permissions:
+* Notwithstanding to the terms of section 5 (5. Conveying Modified Source
+* Versions) and 6 (6. Conveying Non-Source Forms.) of the GNU General
+* Public License version 3, when you create a Related Module, this
+* Related Module is not considered as a part of the work and may be
+* distributed under the license agreement of your choice.
+* A "Related Module" means a set of sources files including their
+* documentation that, without modification of the Source Code, enables
+* supplementary functions or services in addition to those offered by
+* the Software.
+*
+* Rudder is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Rudder.  If not, see <http://www.gnu.org/licenses/>.
+
+*
+*************************************************************************************
+*/
+
+/*
+  Reflect change to create CampaignEvents table from https://issues.rudder.io/issues/21418
+*/
+
+CREATE TABLE CampaignEvents (
+  campaignId   text
+, eventid      text PRIMARY KEY
+, state        text
+, startDate    timestamp with time zone NOT NULL
+, endDate      timestamp with time zone NOT NULL
+, campaignType text
+);
+
