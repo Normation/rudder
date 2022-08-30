537631f9302709804b268a58874d6852d6befd0c
Vincent Membré <vincent.membre@normation.com>
2022-08-30 12:17:33 +0200

    Fixes #21666: Delete group category popup and directive creation popup are not closing

--- webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/components/popup/ModificationValidationPopup.scala
+++ webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/components/popup/ModificationValidationPopup.scala
@@ -421,7 +421,9 @@ class ModificationValidationPopup(
   private[this] def error(msg:String) = <span class="col-lg-12 errors-container">{msg}</span>
 
   private[this] def closePopup() : JsCmd = {
-    JsRaw("""$('#confirmUpdateActionDialog').bsModal('hide');""")
+    JsRaw(
+      """$('#confirmUpdateActionDialog').bsModal('hide');
+        |$('#basePopup').bsModal('hide');""".stripMargin)
   }
 
   /**
