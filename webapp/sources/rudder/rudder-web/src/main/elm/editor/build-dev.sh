b05114358b762187e707d7f3a4b67f296903e40a
Alexis Mousset <alexis.mousset@rudder.io>
2022-11-17 23:49:09 +0100

    fixup! Fixes #22094: Build frontend files with gulp
    Fixes #22094: Build frontend files with gulp

--- webapp/sources/rudder/rudder-web/src/main/elm/editor/build-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# we want that all elm-stuff stay in src/main/elm
-# whatever the path from which this script is called
-ELM_DIR="$( cd "$( dirname "$0" )" && pwd )"
-${ELM_DIR}/../build.sh
