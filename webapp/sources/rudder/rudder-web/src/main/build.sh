2ad0bad2a25069f72ce7e264f7d390ad9a59a642
Alexis Mousset <alexis.mousset@rudder.io>
2022-11-18 10:15:22 +0100

    Fixes #22095: Improve frontend build script with inotify

--- webapp/sources/rudder/rudder-web/src/main/build.sh
+++ webapp/sources/rudder/rudder-web/src/main/build.sh
@@ -1,68 +1,124 @@
-#!/bin/sh
+#!/bin/bash
 
-# Takes a --release parameter for optimization
-# Takes a --quick mode to only copy local sources
+set -e
 
-LOCAL=false
-RELEASE=false
+# Work locally
+BUILD_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ "$1" = "--release" ]; then
-  RELEASE=true
-fi
-if [ "$1" = "--local" ]; then
-  LOCAL=true
-fi
-
-set -ex
-
-JS_OUT="webapp/javascript/"
-JS_LIBS="$JS_OUT/libs/"
-CSS_OUT="webapp/style/"
-CSS_LIBS="$CSS_OUT/libs"
+# Poor man's gulp, with infinitely fewer dependencies
 
 # To add or update a dependency run:
 #
 #   npm install PACKAGE@VERSION
 
-# Work locally
-cd "$(dirname "$0")"
+# Takes a --release parameter for optimization
+# Takes a --watch parameter for watch mode (requires inotifywait installed)
 
-if [ "$RELEASE" = true ]; then
-  # Ensure clean state for release
-  rm -rf $JS_OUT $CSS_OUT node_modules
+WATCH=false
+RELEASE=false
+if [ "$1" = "--release" ]; then
+  RELEASE=true
+fi
+if [ "$1" = "--watch" ]; then
+  WATCH=true
 fi
 
-if [ "$LOCAL" = false ]; then
-  # Ensure correct versions
-  npm ci --omit=dev --no-audit
-fi
+PATH="${BUILD_DIR}/node_modules/.bin:${PATH}"
 
-# JS dependencies
-mkdir -p $JS_LIBS
-# npm
-cp node_modules/*/dist/*.min.js* $JS_LIBS
-cp node_modules/*/js/*.min.js* $JS_LIBS
-cp node_modules/*/dist/**/*.min.js* $JS_LIBS
-cp node_modules/datatables.net-plugins/sorting/natural.js $JS_LIBS/dataTables.naturalSorting.js
-cp node_modules/showdown-xss-filter/showdown-xss-filter.js $JS_LIBS
-# Local JS
-cp -r javascript/* $JS_OUT
-# Local elm
-if [ "$LOCAL" = false ]; then
-  elm/build.sh "$@"
-fi
-# Minify (exclude elm file as they are already minified)
-#
-# Disabled for now as it breaks angular
-#
-#if [ "$RELEASE" = true ]; then
-#  find $JS_OUT \( -name elm -o -name '*.min.js' \) -prune -o -name '*.js' -exec \
-#    terser --mangle --compress --output {} {} \;
-#fi
+ELM_OUT="webapp/javascript/elm/"
 
-# CSS dependencies
-mkdir -p $CSS_LIBS
-# npm
-cp node_modules/*/dist/**/*.min.css* $CSS_LIBS
-# Local CSS
-cp -r style $CSS_OUT
+JS_OUT="webapp/javascript/"
+JS_LIBS="$JS_OUT/libs/"
+
+CSS_OUT="webapp/style/"
+CSS_LIBS="$CSS_OUT/libs/"
+
+success() {
+  printf '\e[1;32m%12s\e[0m %s\n' "$1" "$2"
+}
+
+elm_build() {
+  mkdir -p ${ELM_OUT}
+  cd "${BUILD_DIR}/elm"
+  apps=$(find . -name 'elm.json' -printf "%h\n" | sed "s@\./@@")
+
+  for app in ${apps[*]}; do
+    cd "${BUILD_DIR}/elm/${app}"
+    if [ "$RELEASE" = true ]; then
+        cmd="elm make --optimize sources/${app^}.elm --output=${ELM_OUT}/rudder-${app}.js >/dev/null \
+          && terser ${ELM_OUT}/rudder-${app}.js --compress 'pure_funcs=\"F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9\",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+          |  terser --mangle --output=${ELM_OUT}/rudder-${app}.js"
+    else
+        cmd="elm make sources/${app^}.elm --output=${ELM_OUT}/rudder-${app}.js >/dev/null"
+    fi
+    sem --id front -j+0 "${cmd}"
+  done
+  cd "${BUILD_DIR}"
+}
+
+css() {
+  mkdir -p $CSS_OUT
+  cp -r style/* $CSS_OUT
+  success Copied "local css"
+}
+
+js() {
+  mkdir -p $JS_OUT
+  cp -r javascript/* $JS_OUT
+  success Copied "local js"
+}
+
+vendor_js() {
+  mkdir -p $JS_LIBS
+  cp node_modules/*/dist/*.min.js* $JS_LIBS
+  cp node_modules/*/js/*.min.js* $JS_LIBS
+  cp node_modules/*/dist/**/*.min.js* $JS_LIBS
+  cp node_modules/datatables.net-plugins/sorting/natural.js $JS_LIBS/dataTables.naturalSorting.js
+  cp node_modules/showdown-xss-filter/showdown-xss-filter.js $JS_LIBS
+  success Copied "vendored js"
+}
+
+vendor_css() {
+  mkdir -p $CSS_LIBS
+  cp node_modules/*/dist/**/*.min.css* $CSS_LIBS
+  success Copied "vendored css"
+}
+
+dependencies() {
+  # Silence notice
+  mkdir -p "${HOME}/.parallel"
+  touch "${HOME}/.parallel/will-cite"
+  npm ci --no-audit --silent
+  success Installed "dependencies"
+}
+
+clean() {
+  rm -rf $JS_OUT $CSS_OUT
+  if [ "$RELEASE" = true ]; then
+    rm -rf node_modules
+    rm -rf elm/*/elm-stuff
+  fi
+}
+
+build() {
+  clean
+  dependencies
+  elm_build
+  js
+  css
+  vendor_css
+  vendor_js
+}
+
+build
+
+
+
+
+
+
+#inotifywait -m /path -e create -e moved_to |
+#    while read path action file; do
+#        echo "The file '$file' appeared in directory '$path' via '$action'"
+#        # do something with the file
+#    done
--- webapp/sources/rudder/rudder-web/src/main/elm/build.sh
+++ webapp/sources/rudder/rudder-web/src/main/elm/build.sh
@@ -14,42 +14,5 @@ ELM="elm-0.19.1"
 ELM_DIR="$( cd "$( dirname "$0" )" && pwd )"
 DEST_DIR="${ELM_DIR}/../webapp/javascript/rudder/elm"
 
-if ! command -v ${ELM} &> /dev/null
-then
-  echo "# ERROR: missing ${ELM} binary"
-  echo "# To install the right compiler version:"
-  echo ""
-  echo "$ curl -L -o ${ELM}.gz https://github.com/elm/compiler/releases/download/${ELM}/binary-for-linux-64-bit.gz"
-  echo "$ gzip -d ${ELM}.gz"
-  echo "$ chmod +x ${ELM}"
-  echo "# then put it somewhere in your PATH"
-  exit 1
-fi
 
-build_release() {
-  # Make sure we build from a clean state
-  rm -rf elm-stuff
-  ${ELM} make --optimize sources/${app^}.elm --output=generated/rudder-${app}.js
-  terser generated/rudder-${app}.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | terser --mangle --output=generated/rudder-${app}.min.js
-  # we use the same path for dev and prod so we can't really use .min.js
-  mkdir -p ${DEST_DIR}
-  cp generated/rudder-${app}.min.js ${DEST_DIR}/rudder-${app}.js
-}
 
-build_dev() {
-  ${ELM} make sources/${app^}.elm --output=generated/rudder-${app}.js
-  mkdir -p ${DEST_DIR}
-  cp generated/rudder-${app}.js ${DEST_DIR}/
-}
-
-cd ${ELM_DIR}
-apps=$(find . -name 'elm.json' -printf "%h\n" | sed "s@\./@@")
-
-for app in ${apps[*]}; do
-  cd ${ELM_DIR}/${app}
-  if [ "$1" = "--release" ]; then
-    (set -x; build_release)
-  else
-    (set -x; build_dev)
-  fi
-done
