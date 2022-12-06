73465a66432ba46c2cabbe600a3cb56a995573ba
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-12-07 00:08:39 +0100

    Comment out missing usage of showUsage method in scopt parser

--- webapp/sources/rudder/rudder-templates-cli/src/main/scala/com/normation/templates/cli/TemplateCli.scala
+++ webapp/sources/rudder/rudder-templates-cli/src/main/scala/com/normation/templates/cli/TemplateCli.scala
@@ -139,7 +139,7 @@ object TemplateCli {
 
     // in case of error with args, stop and display usage
     val config = parser.parse(args, Config()).getOrElse {
-      parser.showUsage()
+      // parser.showUsage()
       System.exit(1)
       // just for type inference, never reached
       Config()
