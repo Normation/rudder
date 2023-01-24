f3194cbcebcda23e0e58ce2f00a9a1991746b585
Alexis Mousset <alexis.mousset@rudder.io>
2023-01-24 11:47:07 +0100

    Fixes #22299: Add a line break at the end if inventory rejection log file

--- webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/inventory/InventoryProcessor.scala
+++ webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/inventory/InventoryProcessor.scala
@@ -397,7 +397,7 @@ class InventoryMover(
     // this must never lead to a bubbling failure
     IOResult
       .effect(File(rejectPath).writeText(s"""${date}
-                                            |${result.msg.replaceAll("; cause was:", "\ncause was:")}""".stripMargin))
+                                            |${result.msg.replaceAll("; cause was:", "\ncause was:")}\n""".stripMargin))
       .catchAll(err => InventoryProcessingLogger.error(s"Error when writing rejection log file ${rejectPath}: ${err.fullMsg}"))
       .unit
   }
