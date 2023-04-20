5bba7da6b50819044ee5142da568e5cef142f01c
Félix Dallidet <felix.dallidet@rudder.io>
2023-04-20 16:14:03 +0200

    Fixes #22667: Fix typos in system-updates api doc

--- webapp/sources/api-doc/code_samples/curl/system-updates/campaignEventResult.sh
+++
@@ -1,1 +0,0 @@
-curl --header "X-API-Token: yourToken" --request GET 'https://rudder.example.com/rudder/api/latest/system-update/events/0076a379-f32d-4732-9e91-33ab219d8fde/result/b81d08e8-03f8-46b7-9399-9bea54e41ec7'
