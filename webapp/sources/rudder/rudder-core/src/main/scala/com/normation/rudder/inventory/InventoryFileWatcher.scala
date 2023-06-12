9a6672741bbb6a83b8d535b2a7b8434e73dce356
Francois @fanf42 Armand <francois@fanf.eu>
2023-06-12 15:39:37 +0200

    Fixes #22874: NPE gz inventory file is corrupted

--- webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/inventory/InventoryFileWatcher.scala
+++ webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/inventory/InventoryFileWatcher.scala
@@ -39,8 +39,7 @@ package com.normation.rudder.inventory
 
 import better.files._
 import com.normation.box.IOManaged
-import com.normation.errors.IOResult
-import com.normation.errors.RudderError
+import com.normation.errors._
 import com.normation.inventory.domain.InventoryProcessingLogger
 import com.normation.zio._
 import com.normation.zio.ZioRuntime
@@ -668,6 +667,12 @@ class ProcessFile(
              InventoryProcessingLogger.debug(s"Dealing with zip file '${file.name}'") *>
              IOResult.effect {
                file.unGzipTo(dest)
+             }.catchAll { err => // if the gz file is corrupted, we still want to delete it in the next instruction, but log
+               InventoryProcessingLogger.error(
+                 Chained(s"gz archive '${file.pathAsString}' is corrupted: deleting it.", err).fullMsg
+               )
+             } *>
+             IOResult.effect {
                file.delete()
              }.unit
            } else if (file.name.endsWith(sign)) { // a signature
