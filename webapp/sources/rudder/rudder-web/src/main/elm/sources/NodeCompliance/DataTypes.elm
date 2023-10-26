bb35418e257888fdf6260207bd9e3f2173263ff5
Raphael Gauthier <raphael.gauthier@normation.com>
2023-10-26 14:15:39 +0200

    Fixes #23656: Rewrite Nodes compliance table in Elm

---
+++ webapp/sources/rudder/rudder-web/src/main/elm/sources/NodeCompliance/DataTypes.elm
@@ -0,0 +1,75 @@
+module NodeCompliance.DataTypes exposing (..)
+
+import Dict exposing (Dict)
+import Http exposing (Error)
+
+import Compliance.DataTypes exposing (..)
+
+--
+-- All our data types
+--
+
+type alias RuleId      = { value : String }
+type alias DirectiveId = { value : String }
+type alias NodeId      = { value : String }
+
+
+type alias NodeCompliance =
+  { nodeId            : NodeId
+  , name              : String
+  , compliance        : Float
+  , policyMode        : String
+  , complianceDetails : ComplianceDetails
+  , rules             : List RuleCompliance
+  }
+
+type alias RuleCompliance =
+  { ruleId            : RuleId
+  , name              : String
+  , compliance        : Float
+  , complianceDetails : ComplianceDetails
+  , directives        : List (DirectiveCompliance ValueCompliance)
+  }
+
+type alias DirectiveCompliance value =
+  { directiveId       : DirectiveId
+  , name              : String
+  , compliance        : Float
+  , complianceDetails : ComplianceDetails
+  , components        : List (ComponentCompliance value)
+  }
+
+type alias TableFilters =
+  { sortOrder  : SortOrder
+  , filter     : String
+  , openedRows : Dict String (String, SortOrder)
+  }
+
+type SortOrder = Asc | Desc
+
+type alias UI =
+  { tableFilters      : TableFilters
+  , complianceFilters : ComplianceFilters
+  , loading           : Bool
+  }
+
+type alias Model =
+  { nodeId : NodeId
+  , contextPath : String
+  , policyMode  : String
+  , ui          : UI
+  , nodeCompliance : Maybe NodeCompliance
+  }
+
+type Msg
+  = Ignore
+  | UpdateFilters       TableFilters
+  | UpdateComplianceFilters ComplianceFilters
+  | GoTo                String
+  | ToggleRow           String String
+  | ToggleRowSort       String String SortOrder
+  | GetPolicyModeResult (Result Error String)
+  | GetNodeComplianceResult (Result Error NodeCompliance)
+  | CallApi            (Model -> Cmd Msg)
+  | Refresh
+
