7632c13474584eb09d176730b5cc35be9ec910f1
Alexis Mousset <alexis.mousset@rudder.io>
2023-12-09 17:20:44 +0100

    fixup! fixup! fixup! fixup! fixup! Fixes #23852: Make the (now) various Rust CLIs more consistent
    Fixes #23852: Make the (now) various Rust CLIs more consistent

--- relay/sources/relayd/src/api/shared_folder.rs
+++ relay/sources/relayd/src/api/shared_folder.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later WITH GPL-3.0-linking-source-exception
+// SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: 2019-2020 Normation SAS
 
 use std::{io, sync::Arc};
