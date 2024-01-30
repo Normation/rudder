cd8c0bce6182a483ef659e6207de569288f0bf1d
Francois @fanf42 Armand <francois@fanf.eu>
2024-01-30 22:18:49 +0100

    Fixes #24105: Machine is not correctly create with node create API

--- webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/services/servers/NewNodeManager.scala
+++ webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/services/servers/NewNodeManager.scala
@@ -900,10 +900,12 @@ class AcceptHostnameAndIp(
       // if not, we don't group them that the duplicate appears in the list
       noDuplicatesH <- if (duplicatesH.isEmpty) Full({})
                        else {
-                         val startMessage =
-                           if (duplicatesH.size >= 2) "There are already ${duplicatesH.size} nodes" else "There is already a node"
+                         val startMessage = {
+                           if (duplicatesH.size >= 2) s"There are already ${duplicatesH.size} nodes"
+                           else "There is already a node"
+                         }
                          Failure(
-                           s"${startMessage} with hostname '${name}' in Rudder. You can not add it again."
+                           s"${startMessage} with hostname '${hostnames.mkString(", ")}' in Rudder. You can not add it again."
                          )
                        }
     } yield {}
