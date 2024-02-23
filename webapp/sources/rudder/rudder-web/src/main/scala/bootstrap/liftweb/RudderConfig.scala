cb46ad51fd4f7f0c7bee29e098e835a381447a5c
Clark Andrianasolo <65616064+clarktsiory@users.noreply.github.com>
2024-02-23 16:30:17 +0100

    Fixes #24203: Role API mapping should be exposed in RudderConfig (#5401)

--- webapp/sources/rudder/rudder-web/src/main/scala/bootstrap/liftweb/RudderConfig.scala
+++ webapp/sources/rudder/rudder-web/src/main/scala/bootstrap/liftweb/RudderConfig.scala
@@ -1133,6 +1133,7 @@ object RudderConfig extends Loggable {
   val restDataSerializer:                  RestDataSerializer                         = rci.restDataSerializer
   val restExtractorService:                RestExtractorService                       = rci.restExtractorService
   val restQuicksearch:                     RestQuicksearch                            = rci.restQuicksearch
+  val roleApiMapping:                      RoleApiMapping                             = rci.roleApiMapping
   val roAgentRunsRepository:               RoReportsExecutionRepository               = rci.roAgentRunsRepository
   val roApiAccountRepository:              RoApiAccountRepository                     = rci.roApiAccountRepository
   val roDirectiveRepository:               RoDirectiveRepository                      = rci.roDirectiveRepository
@@ -1291,6 +1292,7 @@ case class RudderServiceApi(
     jsonPluginDefinition:                ReadPluginPackageInfo,
     rudderApi:                           LiftHandler,
     authorizationApiMapping:             ExtensibleAuthorizationApiMapping,
+    roleApiMapping:                      RoleApiMapping,
     roRuleCategoryRepository:            RoRuleCategoryRepository,
     woRuleCategoryRepository:            WoRuleCategoryRepository,
     workflowLevelService:                DefaultWorkflowLevel,
@@ -3583,6 +3585,7 @@ object RudderConfigInit {
       jsonPluginDefinition,
       rudderApi,
       authorizationApiMapping,
+      roleApiMapping,
       roRuleCategoryRepository,
       woRuleCategoryRepository,
       workflowLevelService,
