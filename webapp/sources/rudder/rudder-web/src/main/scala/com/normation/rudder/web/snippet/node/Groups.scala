78903b8a793eb40b8da68c0da8d5fee0ebaef96e
Clark Andrianasolo <clark.andrianasolo@rudder.io>
2024-03-18 10:27:23 +0100

    fixup! Fixes #24416: Elm properties app is loaded multiple times
    Fixes #24416: Elm properties app is loaded multiple times

--- webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/snippet/node/Groups.scala
+++ webapp/sources/rudder/rudder-web/src/main/scala/com/normation/rudder/web/snippet/node/Groups.scala
@@ -482,36 +482,38 @@ class Groups extends StatefulSnippet with DefaultExtendableSnippet[Groups] with 
              |// When the tab is shown we need to initialize the Elm app
              |$$('a[href="#groupPropertiesTab"]').on('show.bs.tab', function() {
              |  var main = document.getElementById("nodeproperties-app")
-             |  var initValues = {
-             |      contextPath    : "${S.contextPath}"
-             |    , hasNodeWrite   : CanWriteNode
-             |    , hasNodeRead    : CanReadNode
-             |    , nodeId         : '${value}'
-             |    , objectType     : 'group'
-             |  };
-             |  var app = Elm.Nodeproperties.init({node: main, flags: initValues});
-             |  app.ports.successNotification.subscribe(function(str) {
-             |    createSuccessNotification(str)
-             |  });
-             |  app.ports.errorNotification.subscribe(function(str) {
-             |    createErrorNotification(str)
-             |  });
-             |  // Initialize tooltips
-             |  app.ports.initTooltips.subscribe(function(msg) {
-             |    setTimeout(function(){
-             |      $$('.bs-tooltip').bsTooltip();
-             |    }, 400);
-             |  });
-             |  app.ports.copy.subscribe(function(str) {
-             |    navigator.clipboard.writeText(str);
-             |  });
-             |  app.ports.initInputs.subscribe(function(str) {
-             |    setTimeout(function(){
-             |      $$(".auto-resize").on("input", autoResize).each(function(){
-             |        autoResize(this);
-             |      });
-             |    }, 10);
-             |  });
+             |  if (main) {
+             |    var initValues = {
+             |        contextPath    : "${S.contextPath}"
+             |      , hasNodeWrite   : CanWriteNode
+             |      , hasNodeRead    : CanReadNode
+             |      , nodeId         : '${value}'
+             |      , objectType     : 'group'
+             |    };
+             |    var app = Elm.Nodeproperties.init({node: main, flags: initValues});
+             |    app.ports.successNotification.subscribe(function(str) {
+             |      createSuccessNotification(str)
+             |    });
+             |    app.ports.errorNotification.subscribe(function(str) {
+             |      createErrorNotification(str)
+             |    });
+             |    // Initialize tooltips
+             |    app.ports.initTooltips.subscribe(function(msg) {
+             |      setTimeout(function(){
+             |        $$('.bs-tooltip').bsTooltip();
+             |      }, 400);
+             |    });
+             |    app.ports.copy.subscribe(function(str) {
+             |      navigator.clipboard.writeText(str);
+             |    });
+             |    app.ports.initInputs.subscribe(function(str) {
+             |      setTimeout(function(){
+             |        $$(".auto-resize").on("input", autoResize).each(function(){
+             |          autoResize(this);
+             |        });
+             |      }, 10);
+             |    });
+             |  }
              |});
              |""".stripMargin)
 
