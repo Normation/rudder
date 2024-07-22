98e2afcdc6d0ccd774226ae2801c492c9b4d1d61
ElaadF <elaad.f@gmail.com>
2024-07-22 21:25:23 +0200

    Fixes #24634: API to find usage of a node property in Directives

--- webapp/sources/rudder/rudder-web/src/main/elm/sources/NodeProperties/Init.elm
+++ webapp/sources/rudder/rudder-web/src/main/elm/sources/NodeProperties/Init.elm
@@ -5,13 +5,11 @@ import Dict exposing (Dict)
 import NodeProperties.DataTypes exposing (..)
 import NodeProperties.ApiCalls exposing (getNodeProperties)
 
-import Ui.Datatable exposing (defaultTableFilters)
-
-
 init : { contextPath : String, hasNodeWrite : Bool, hasNodeRead : Bool, nodeId : String, objectType : String} -> ( Model, Cmd Msg )
 init flags =
   let
-    initUi = UI flags.hasNodeWrite flags.hasNodeRead True NoModal Dict.empty [] (defaultTableFilters Name)
+    pagination = TablePagination 1 1 10 0
+    initUi = UI flags.hasNodeWrite flags.hasNodeRead True NoModal Dict.empty [] (TableFiltersOnProperty Name Asc "") (TableFiltersOnUsage Name NodeProperties.DataTypes.Asc "" Directives pagination)
     initModel = Model flags.contextPath flags.nodeId flags.objectType [] (EditProperty "" "" StringFormat True True False) initUi
   in
     ( initModel
