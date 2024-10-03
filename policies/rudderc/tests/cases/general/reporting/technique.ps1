2671e597b3e561ada5d0a9db5aef0d388d685a86
Alexis Mousset <alexis.mousset@rudder.io>
2024-10-03 20:51:28 +0200

    Fixes #25600: Compliance reporting with type worst-case-percent is not known by rudderc

--- policies/rudderc/tests/cases/general/reporting/technique.ps1
+++ policies/rudderc/tests/cases/general/reporting/technique.ps1
@@ -410,5 +410,101 @@ ntp
         Compute-Method-Call @fallBackReportParams -PolicyMode $policyMode -ReportId $reportId -DisableReporting:$true -MethodCall $failedCall
     }
 
+    $reportId=$reportIdBase + "df06e919-02b7-41a7-a03f-4239592f3c21"
+    try {
+        $componentKey = 'ntp'
+        $reportParams = @{
+            ClassPrefix = ([Rudder.Condition]::canonify(("package_present_" + $componentKey)))
+            ComponentKey = $componentKey
+            ComponentName = 'Enabled reporting'
+            PolicyMode = $policyMode
+            ReportId = $reportId
+            DisableReporting = $false
+            TechniqueName = $techniqueName
+        }
+        
+        $methodParams = @{
+            Architecture = ''
+            Name = @'
+ntp
+'@
+            Provider = ''
+            Version = @'
+
+'@
+            
+        }
+        $call = Package-Present @methodParams -PolicyMode $policyMode
+        Compute-Method-Call @reportParams -MethodCall $call
+        
+    } catch [Nustache.Core.NustacheDataContextMissException] {
+        $failedCall = [Rudder.MethodResult]::Error(
+            ([String]::Format(
+                'The method call was skipped because it references an undefined variable "{0}"',
+                $_.ToString()
+            )),
+            $techniqueName
+        )
+        Compute-Method-Call @fallBackReportParams -PolicyMode $policyMode -ReportId $reportId -DisableReporting:$false -MethodCall $failedCall
+    } catch {
+        $failedCall = [Rudder.MethodResult]::Error(
+            [Rudder.MethodStatus]::Error,
+            ([String]::Format(
+                'The method call was skipped as an unexpected error was thrown "{0}"',
+                (Format-Exception $_)[1]
+            )),
+            $techniqueName
+        )
+        Compute-Method-Call @fallBackReportParams -PolicyMode $policyMode -ReportId $reportId -DisableReporting:$false -MethodCall $failedCall
+    }
+
+    $reportId=$reportIdBase + "d76686bb-79ab-4ae5-b45f-108492ab4101"
+    try {
+        $componentKey = 'ntp'
+        $reportParams = @{
+            ClassPrefix = ([Rudder.Condition]::canonify(("package_present_" + $componentKey)))
+            ComponentKey = $componentKey
+            ComponentName = 'Disabled reporting'
+            PolicyMode = $policyMode
+            ReportId = $reportId
+            DisableReporting = $true
+            TechniqueName = $techniqueName
+        }
+        
+        $methodParams = @{
+            Architecture = ''
+            Name = @'
+ntp
+'@
+            Provider = ''
+            Version = @'
+
+'@
+            
+        }
+        $call = Package-Present @methodParams -PolicyMode $policyMode
+        Compute-Method-Call @reportParams -MethodCall $call
+        
+    } catch [Nustache.Core.NustacheDataContextMissException] {
+        $failedCall = [Rudder.MethodResult]::Error(
+            ([String]::Format(
+                'The method call was skipped because it references an undefined variable "{0}"',
+                $_.ToString()
+            )),
+            $techniqueName
+        )
+        Compute-Method-Call @fallBackReportParams -PolicyMode $policyMode -ReportId $reportId -DisableReporting:$true -MethodCall $failedCall
+    } catch {
+        $failedCall = [Rudder.MethodResult]::Error(
+            [Rudder.MethodStatus]::Error,
+            ([String]::Format(
+                'The method call was skipped as an unexpected error was thrown "{0}"',
+                (Format-Exception $_)[1]
+            )),
+            $techniqueName
+        )
+        Compute-Method-Call @fallBackReportParams -PolicyMode $policyMode -ReportId $reportId -DisableReporting:$true -MethodCall $failedCall
+    }
+
     EndTechniqueCall -Name $techniqueName
 }
