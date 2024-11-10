a509518ba312627164d1353f51554c0ecd8675c3
Alexis Mousset <alexis.mousset@rudder.io>
2024-11-10 12:26:10 +0100

    fixup! Fixes #25843: Update Rust dependencies
    Fixes #25843: Update Rust dependencies

--- relay/sources/relayd/benches/benches.rs
+++ relay/sources/relayd/benches/benches.rs
@@ -98,7 +98,7 @@ fn bench_uncompress_runlog(c: &mut Criterion) {
 pub fn db() -> PgPool {
     let db_config = DatabaseConfig {
         url: "postgres://rudderreports@127.0.0.1/rudder".to_string(),
-        password: SecretString::new("PASSWORD".to_string()),
+        password: "PASSWORD".into(),
         max_pool_size: 10,
     };
     pg_pool(&db_config).unwrap()
