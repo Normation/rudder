6d4db216a73290832b2b6550f48cb63710ef0582
Alexis Mousset <alexis.mousset@rudder.io>
2024-12-16 20:42:30 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Fixes #26077: Test cleanup after ncf merge into Rudder - next
    Fixes #26077: Test cleanup after ncf merge into Rudder - next

--- policies/lib/tests/quick/nbsp_is_not_allowed.sh
+++ policies/lib/tests/quick/nbsp_is_not_allowed.sh
@@ -13,9 +13,9 @@ FIND_ARGS="-type f -not -wholename */.git/* -not -wholename */api/flask/* -not -
 while read line
 do
   export FIND_ARGS="${FIND_ARGS} -not -wholename */${line}"
-done < $GIT_ROOT/.gitignore
+done < $NCF_ROOT/../.gitignore
 
-ALL_TESTS=`find ${GIT_ROOT} ${FIND_ARGS}`
+ALL_TESTS=`find ${NCF_ROOT}/.. ${FIND_ARGS}`
 
 ERRORS=0
 for file in ${ALL_TESTS}
