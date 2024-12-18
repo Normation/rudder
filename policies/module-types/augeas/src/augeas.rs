0dab271f28c2ae47caa18bcebd7af37c066d1912
Alexis Mousset <alexis.mousset@rudder.io>
2024-12-18 13:47:44 +0100

    Fixes #25942: Create the augeas module (#6043)

---
+++ policies/module-types/augeas/src/augeas.rs
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2024 Normation SAS
+
+use crate::AugeasParameters;
+use raugeas::{CommandsNumber, Flags, SaveMode};
+use rudder_module_type::{rudder_debug, CheckApplyResult, Outcome, PolicyMode};
+use std::env;
+
+pub struct Augeas {}
+
+impl Augeas {
+    pub(crate) fn new() -> anyhow::Result<Self> {
+        // Cleanup the environment first to avoid any interference.
+        //
+        // SAFETY: Safe as the module is single threaded.
+        unsafe {
+            env::remove_var("AUGEAS_ROOT");
+            env::remove_var("AUGEAS_LENS_LIB");
+            env::remove_var("AUGEAS_DEBUG");
+        }
+
+        Ok(Augeas {})
+    }
+
+    pub(crate) fn handle_check_apply(
+        &self,
+        p: AugeasParameters,
+        policy_mode: PolicyMode,
+    ) -> CheckApplyResult {
+        let mut flags = Flags::NONE;
+
+        if let Some(l) = p.lens.as_ref() {
+            rudder_debug!("Using lens: {l}, skipping autoloading");
+            flags.insert(Flags::NO_MODULE_AUTOLOAD);
+        } else {
+            // We never want to load the whole tree.
+            rudder_debug!("Autoloading lenses");
+            flags.insert(Flags::NO_LOAD);
+        }
+
+        if p.type_check_lenses {
+            rudder_debug!("Type checking lenses");
+            flags.insert(Flags::TYPE_CHECK);
+        }
+
+        let mut aug = raugeas::Augeas::init(p.root.as_deref(), &p.load_paths(), flags)?;
+
+        // Show version for debugging purposes.
+        let version = aug.version()?;
+        rudder_debug!("Augeas version: {}", version);
+
+        //////////////////////////////////
+        // Start with the special unsafe mode
+        //////////////////////////////////
+
+        if !p.commands.is_empty() {
+            // Only allowed in "enforce" mode.
+            // Makes little sense for audit mode, and risks making changes.
+            // Already validated but make sure.
+            assert_eq!(policy_mode, PolicyMode::Enforce);
+
+            rudder_debug!("Running commands: {:?}", p.commands);
+            let (num, out) = aug.srun(&p.commands.join("\n"))?;
+            let summary = match num {
+                CommandsNumber::Success(n) => format!("{n} success"),
+                CommandsNumber::Quit => "has quit".to_string(),
+            };
+            rudder_debug!("{summary}, output: {out}");
+            return Ok(Outcome::repaired(summary));
+        }
+
+        //////////////////////////////////
+        // Now in "normal" mode:
+        //
+        // * We only work on one file.
+        // * We handle backups, diffs, precise reporting.
+        // * We guarantee that the policy mode is respected.
+        //////////////////////////////////
+
+        let path = p.path.clone().unwrap();
+
+        if let Some(l) = p.lens() {
+            // If we have a lens, we need to load it and load the file.
+            aug.set(&format!("/augeas/load/${l}/lens"), &l.to_string())?;
+            aug.set(&format!("/augeas/load/${l}/incl"), &path.to_string())?;
+            aug.load()?;
+        } else {
+            // Else load it with the detected lens.
+            // FIXME handle errors.
+            aug.load_file(&path.to_string())?;
+        }
+
+        if !p.changes.is_empty() {
+            rudder_debug!("Running changes: {:?}", p.changes);
+            todo!()
+        }
+        if !p.checks.is_empty() {
+            rudder_debug!("Running checks: {:?}", p.checks);
+            todo!()
+        }
+
+        match policy_mode {
+            PolicyMode::Enforce => {
+                aug.set_save_mode(SaveMode::Backup)?;
+            }
+            PolicyMode::Audit => {
+                aug.set_save_mode(SaveMode::Noop)?;
+            }
+        }
+        aug.save()?;
+
+        // Get information about changes
+
+        // make backups
+
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+    use tempfile::tempdir;
+
+    fn arguments(commands: Vec<String>) -> AugeasParameters {
+        AugeasParameters {
+            commands,
+            ..Default::default()
+        }
+    }
+
+    #[test]
+    fn it_writes_file_from_commands() {
+        let augeas = Augeas::new().unwrap();
+        let d = tempdir().unwrap().into_path();
+        let f = d.join("test");
+        let lens = "Simplelines";
+
+        let r = augeas
+            .handle_check_apply(
+                arguments(vec![
+                    format!("set /augeas/load/${lens}/lens \"{lens}.lns\""),
+                    format!("set /augeas/load/${lens}/incl \"{}\"", f.display()),
+                    "load".to_string(),
+                    format!("set /files{}/0 \"hello world\"", f.display()),
+                    "save".to_string(),
+                ]),
+                PolicyMode::Enforce,
+            )
+            .unwrap();
+        assert_eq!(r, Outcome::repaired("5 success".to_string()));
+
+        let content = read_to_string(&f).unwrap();
+        assert_eq!(content.trim(), "hello world");
+    }
+}
---
+++ policies/module-types/augeas/src/check.rs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2024 Normation SAS
+
+//! Implements augeas-based checks, used either for audits or conditions.
+
+/*
+pub enum Constraint {
+    /// Is present
+    Present,
+    /// Is absent
+    Absent,
+    /// Equals a value
+    Equal,
+    /// Does not equal a value
+    NotEqual,
+    /// Match a regular expression
+    Match,
+    /// Does not match a regular expression
+    NotMatch,
+    /// Is greater than a value
+    GreaterThan,
+    /// Is greater than or equal to a value
+    GreaterThanOrEqual,
+    /// Is less than a value
+    LessThan,
+    /// Is less than or equal to a value
+    LessThanOrEqual,
+    // Is in a list of values
+    InList(Vec<String>),
+    // Is not in a list of forbidden values
+    NotInList(Vec<String>),
+}
+*/
