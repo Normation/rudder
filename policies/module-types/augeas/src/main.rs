ee46e3f6dda22ebb5dc5a1e1a981c1704ae3457d
Alexis Mousset <alexis.mousset@rudder.io>
2025-01-15 13:55:26 +0100

    fixup! fixup! Fixes #26089: Implement augeas module
    Fixes #26089: Implement augeas module

--- policies/module-types/augeas/src/main.rs
+++ policies/module-types/augeas/src/main.rs
@@ -3,29 +3,6 @@
 
 use anyhow::Result;
 
-// transofrmer un diff en conf augeas??
-// outil pour savoir comment écrire le set ??
-// ou pour prendre l'état d'un fichier ?
-
-// voir si les span peuvent ider à faire du debug/reporting
-
-// diff de valeur pour les audits TODO
-
-// implémenter une commande pour le diff!
-// intercepter le help et le modifier!
-
-// Pourquoi pas du srun : pour pouvoir interrompre au premier problème
-
-// très important!
-// https://github.com/hercules-team/augeas/issues/68
-// https://github.com/dominikh/go-augeas/blob/master/augeas.go
-
-// diff de valeur d'audit expected vs constraint
-
-// diff de valeur sur les set ??
-
-// observabilité d'augeas !
-
 fn main() -> Result<(), anyhow::Error> {
     rudder_module_augeas::entry()
 }
