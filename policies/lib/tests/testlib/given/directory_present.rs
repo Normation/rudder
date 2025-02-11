4ee491a9812a045eea715e5ba4ac26af06bcdc16
Fdall <felix.dallidet@rudder.io>
2025-02-11 10:38:21 +0100

    Fixes #26340: Write a new test framework for the Rudder methods

---
+++ policies/lib/tests/testlib/given/directory_present.rs
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 Normation SAS
+
+use crate::testlib::given::setup_state::TestSetup;
+use crate::testlib::test_setup::TestSetupResult;
+use anyhow::Error;
+use log::debug;
+use std::fs;
+
+#[derive(Clone, Debug)]
+pub struct DirectoryPresentStruct {
+    pub path: String,
+}
+impl TestSetup for DirectoryPresentStruct {
+    fn resolve(&self) -> anyhow::Result<TestSetupResult, Error> {
+        debug!("Creating directory {}", self.path);
+        fs::create_dir(&self.path)?;
+        Ok(TestSetupResult::default())
+    }
+}
---
+++ policies/lib/tests/testlib/given/setup_state.rs
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 Normation SAS
+
+use crate::testlib::given::directory_present::DirectoryPresentStruct;
+use crate::testlib::given::file_absent::FileAbsentStruct;
+use crate::testlib::given::file_present::FilePresentStruct;
+use crate::testlib::test_setup::TestSetupResult;
+use anyhow::Error;
+
+pub trait TestSetup {
+    fn resolve(&self) -> anyhow::Result<TestSetupResult, Error>;
+}
+
+#[derive(Clone)]
+pub enum SetupState {
+    FilePresent(FilePresentStruct),
+    FileAbsent(FileAbsentStruct),
+    DirectoryPresent(DirectoryPresentStruct),
+}
+
+impl TestSetup for SetupState {
+    fn resolve(&self) -> anyhow::Result<TestSetupResult, Error> {
+        match self {
+            SetupState::FilePresent(fp) => fp.resolve(),
+            SetupState::FileAbsent(fa) => fa.resolve(),
+            SetupState::DirectoryPresent(dp) => dp.resolve(),
+        }
+    }
+}
