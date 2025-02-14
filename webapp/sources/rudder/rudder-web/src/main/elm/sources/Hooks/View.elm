761c76ba1e7ae2aeb278c989ba9f27bb3f9e7d11
Raphael Gauthier <raphael.gauthier@normation.com>
2025-02-14 15:55:43 +0100

    Fixes #26359: Fix display problems related to menu changes in 8.3

--- webapp/sources/rudder/rudder-web/src/main/elm/sources/Hooks/View.elm
+++ webapp/sources/rudder/rudder-web/src/main/elm/sources/Hooks/View.elm
@@ -10,34 +10,26 @@ import Hooks.ViewUtils exposing (..)
 
 view : Model -> Html Msg
 view model =
-  div[ class "rudder-template"]
-  [ div[ class "one-col"]
-    [ div[ class "main-header"]
-      [ div[ class "header-title"]
-        [ h1[]
-          [ span[] [text "Hooks"]
-          ]
-        ]
-      , div [class "header-description"]
-        [ p[][text "This page shows you the current hooks used in Rudder."]
-        , p[][
-            text "For more information, please consult the"
-          , a [href "/rudder-doc/reference/7.2/usage/advanced_configuration_management.html#_server_event_hooks"][text " documentation."]
-          ]
+  div[class "d-flex flex-column"]
+  [ div [class "callout-fade callout-info"]
+    [ div[class "marker"]
+      [ span [class "fa fa-info-circle"][]
+      ]
+    , p[][text "This page shows you the current hooks used in Rudder."]
+    , p[][
+        text "For more information, please consult the"
+      , a [href "/rudder-doc/reference/7.2/usage/advanced_configuration_management.html#_server_event_hooks"][text " documentation."]
+      ]
+    ]
+  , div[class "d-flex"]
+    [ div [class "template-sidebar sidebar-left"]
+      [ div [id "navbar-scrollspy", class "sidebar-body"]
+        [ displayNavList model.categories
         ]
       ]
-    , div[ class "one-col-main"]
-      [ div [class "template-sidebar sidebar-left"]
-        [ div [id "navbar-scrollspy", class "sidebar-body"]
-          [ displayNavList model.categories
-          ]
-        ]
-      , div[ class "template-main"]
-        [ div[ class "main-container"]
-          [ div[ class "main-details", attribute "data-bs-spy" "scroll", attribute "data-bs-target" "#navbar-scrollspy", attribute "data-bs-smooth-scroll" "true"]
-            [ displayHooksList model.root model.categories
-            ]
-          ]
+    , div[ class "template-main"]
+      [ div[ attribute "data-bs-spy" "scroll", attribute "data-bs-target" "#navbar-scrollspy", attribute "data-bs-smooth-scroll" "true"]
+        [ displayHooksList model.root model.categories
         ]
       ]
     ]
