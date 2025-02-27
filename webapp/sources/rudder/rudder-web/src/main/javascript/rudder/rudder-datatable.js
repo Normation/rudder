0968a755f067e16657c8f8bad1e41e08b8d50c62
Raphael Gauthier <raphael.gauthier@normation.com>
2025-02-27 16:58:03 +0100

    Fixes #26354: Adding a property column to nodes list causes lines to double height thus screen shows 2 times less nodes

--- webapp/sources/rudder/rudder-web/src/main/javascript/rudder/rudder-datatable.js
+++ webapp/sources/rudder/rudder-web/src/main/javascript/rudder/rudder-datatable.js
@@ -1152,8 +1152,8 @@ function propertyFunction(value, inherited) { return function (nTd, sData, oData
       provider = $('<span class="rudder-label label-provider label-sm" data-bs-toggle="tooltip" data-bs-placement="right">inherited</span>')
       provider.attr('title', "This property is inherited from these group(s) or global parameter: <div>"+ property.hierarchy + "</div>.")
     }
-    var pre = $("<pre onclick='$(this).toggleClass(\"toggle\")' class='json-beautify show-more'></pre>").text(text);
-    $(nTd).prepend( pre ).prepend(provider)
+    var pre = $("<pre onclick='$(this).toggleClass(\"toggle\")' class='json-beautify show-more'></pre>").text(text).prepend(provider);
+    $(nTd).prepend( pre );
   }
 } }
 
