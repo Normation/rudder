8e34304066e524066632d7217eb0f7d3fe3ba0c5
Martin Hart <114212671+m4rtinh4rt@users.noreply.github.com>
2025-03-10 15:48:29 +0100

    Fixes #26507: Adds tests to the template module CLI

---
+++ policies/module-types/template/tests/cli.rs
@@ -0,0 +1,45 @@
+use assert_cmd::prelude::*;
+use std::process::Command;
+use tempfile::NamedTempFile;
+
+#[test]
+fn test_default_template_engine() -> Result<(), Box<dyn std::error::Error>> {
+    // The default template engine is set to minijinja.
+    let output_file = NamedTempFile::new()?;
+    let mut cmd = Command::cargo_bin("rudder-module-template")?;
+
+    cmd.arg("--template").arg("tests/template.j2");
+    cmd.arg("--data").arg("tests/data.json");
+    cmd.arg("--out").arg(output_file.path());
+    cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_jinja_template_engine() -> Result<(), Box<dyn std::error::Error>> {
+    let output_file = NamedTempFile::new()?;
+    let mut cmd = Command::cargo_bin("rudder-module-template")?;
+
+    cmd.arg("--template").arg("tests/template.j2");
+    cmd.arg("--data").arg("tests/data.json");
+    cmd.arg("--out").arg(output_file.path());
+    cmd.arg("--engine").arg("minijinja");
+    cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_mustache_template_engine() -> Result<(), Box<dyn std::error::Error>> {
+    let output_file = NamedTempFile::new()?;
+    let mut cmd = Command::cargo_bin("rudder-module-template")?;
+
+    cmd.arg("--template").arg("tests/template.mustache");
+    cmd.arg("--data").arg("tests/data.json");
+    cmd.arg("--out").arg(output_file.path());
+    cmd.arg("--engine").arg("mustache");
+    cmd.assert().success();
+
+    Ok(())
+}
