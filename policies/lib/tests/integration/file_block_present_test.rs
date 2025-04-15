c49adce6a13cbaa7b1a5665942619f6e73bd8ca3
Fdall <felix.dallidet@rudder.io>
2025-04-15 13:01:15 +0200

    fixup! fixup! fixup! fixup! Fixes #26736: Port to log v4 and test more legacy methods
    Fixes #26736: Port to log v4 and test more legacy methods

--- policies/lib/tests/integration/file_block_present_test.rs
+++ policies/lib/tests/integration/file_block_present_test.rs
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: 2025 Normation SAS
 
-use std::fs;
 use crate::integration::{end_test, get_lib_path, init_test};
 use crate::testlib::given::Given;
 use crate::testlib::method_test_suite::MethodTestSuite;
 use crate::testlib::method_to_test::{method, MethodStatus};
+use std::fs;
 
 #[test]
 fn it_should_edit_the_file_if_needed() {
@@ -13,7 +13,11 @@ fn it_should_edit_the_file_if_needed() {
     let file = workdir.path().join("file_to_edit");
     let file_path = file.clone().to_string_lossy().into_owned();
 
-    let tested_method = &method("file_block_present", &[&file_path, "A block to add\non multiple lines"]).enforce();
+    let tested_method = &method(
+        "file_block_present",
+        &[&file_path, "A block to add\non multiple lines"],
+    )
+    .enforce();
     let r = MethodTestSuite::new()
         .given(Given::file_present(&file_path, "Hello world!"))
         .when(tested_method)
@@ -40,7 +44,11 @@ fn it_should_error_in_audit_mode_if_the_file_is_not_correct() {
     let file = workdir.path().join("file_to_edit");
     let file_path = file.clone().to_string_lossy().into_owned();
 
-    let tested_method = &method("file_block_present", &[&file_path, "A block to add\non multiple lines"]).audit();
+    let tested_method = &method(
+        "file_block_present",
+        &[&file_path, "A block to add\non multiple lines"],
+    )
+    .audit();
     let r = MethodTestSuite::new()
         .given(Given::file_present(&file_path, "Hello world!"))
         .when(tested_method)
@@ -65,14 +73,25 @@ fn it_should_be_idempotent() {
     let file = workdir.path().join("file_to_edit");
     let file_path = file.clone().to_string_lossy().into_owned();
 
-    let tested_method = &method("file_block_present", &[&file_path, "A block to add\non multiple lines"]).enforce();
-    let tested_method2 = &method("file_block_present", &[&file_path, "A block to add\non multiple lines"]).enforce();
+    let tested_method = &method(
+        "file_block_present",
+        &[&file_path, "A block to add\non multiple lines"],
+    )
+    .enforce();
+    let tested_method2 = &method(
+        "file_block_present",
+        &[&file_path, "A block to add\non multiple lines"],
+    )
+    .enforce();
     let r = MethodTestSuite::new()
         .given(Given::file_present(&file_path, "Hello world!"))
         .given(Given::method_call(tested_method2))
         .when(tested_method)
         .execute(get_lib_path(), workdir.path().to_path_buf());
-    r.assert_legacy_result_conditions(tested_method, vec![MethodStatus::Repaired, MethodStatus::Success]);
+    r.assert_legacy_result_conditions(
+        tested_method,
+        vec![MethodStatus::Repaired, MethodStatus::Success],
+    );
     r.assert_log_v4_result_conditions(tested_method, MethodStatus::Success);
     assert!(
         file.exists(),
