1a91d05cf868cfeac9b93b25a12ec50fae273118
Martin Hart <114212671+m4rtinh4rt@users.noreply.github.com>
2025-05-22 15:58:03 +0200

    Fixes #26930: Adding a Windows runner from GitHub Actions to the template module (#6387)

--- policies/module-types/template/tests/minijinja.rs
+++ policies/module-types/template/tests/minijinja.rs
@@ -1,20 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Normation SAS
 
+#[cfg(target_family = "unix")]
 use std::{
     env,
     fs::{self, read_to_string},
     path::Path,
 };
 
+#[cfg(target_family = "unix")]
 use anyhow::anyhow;
+#[cfg(target_family = "unix")]
 use rudder_commons_test::module_type::unix;
+#[cfg(target_family = "unix")]
 use rudder_module_type::{Outcome, PolicyMode};
+#[cfg(target_family = "unix")]
 use tempfile::tempdir;
 
+#[cfg(target_family = "unix")]
 const BIN: &str = concat!("../../../target/debug/", env!("CARGO_PKG_NAME"));
 
 #[test]
+#[cfg(target_family = "unix")]
 fn it_renders_minijinja_inlined() {
     let root_dir = tempdir().unwrap();
     let test_path = root_dir.path().join("output");
@@ -34,6 +41,7 @@ fn it_renders_minijinja_inlined() {
 }
 
 #[test]
+#[cfg(target_family = "unix")]
 fn it_fails_on_undefined_values() {
     let root_dir = tempdir().unwrap();
     let test_path = root_dir.path().join("output");
@@ -52,6 +60,7 @@ fn it_fails_on_undefined_values() {
 }
 
 #[test]
+#[cfg(target_family = "unix")]
 fn it_renders_minijinja_from_file() {
     let root_dir = tempdir().unwrap();
     let test_path = root_dir.path().join("output");
@@ -71,6 +80,7 @@ fn it_renders_minijinja_from_file() {
 }
 
 #[test]
+#[cfg(target_family = "unix")]
 fn it_checks_minijinja() {
     let root_dir = tempdir().unwrap();
     let test_path = root_dir.path().join("output");
@@ -89,6 +99,7 @@ fn it_checks_minijinja() {
 }
 
 #[test]
+#[cfg(target_family = "unix")]
 fn it_checks_correct_minijinja() {
     let root_dir = tempdir().unwrap();
     let test_path = root_dir.path().join("output");
