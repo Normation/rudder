82b3914df45759abc4d43816c9208e8b708e4f8c
Francois @fanf42 Armand <francois@fanf.eu>
2025-06-04 14:18:12 +0200

    The expected serialization in test is incorrect

--- webapp/sources/rudder/rudder-rest/src/test/scala/com/normation/rudder/domain/reports/JsonPostresqlSerializationTest.scala
+++ webapp/sources/rudder/rudder-rest/src/test/scala/com/normation/rudder/domain/reports/JsonPostresqlSerializationTest.scala
@@ -317,9 +317,7 @@ object ExpectedJson {
       |                                    "rtid" : "reportId-check2.1",
       |                                    "msrs" : [
       |                                      {
-      |                                        "rt" : {
-      |                                          "EnforceRepaired" : {}
-      |                                        },
+      |                                        "rt" : "EnforceRepaired",
       |                                        "m" : "check 2.1 is repaired"
       |                                      }
       |                                    ]
@@ -338,9 +336,7 @@ object ExpectedJson {
       |                                    "rtid" : "reportId-check2.2",
       |                                    "msrs" : [
       |                                      {
-      |                                        "rt" : {
-      |                                          "EnforceRepaired" : {}
-      |                                        },
+      |                                        "rt" : "EnforceRepaired",
       |                                        "m" : "check 2.2 is repaired"
       |                                      }
       |                                    ]
@@ -367,9 +363,7 @@ object ExpectedJson {
       |                                    "rtid" : "reportId-check3",
       |                                    "msrs" : [
       |                                      {
-      |                                        "rt" : {
-      |                                          "AuditCompliant" : {}
-      |                                        },
+      |                                        "rt" : "AuditCompliant",
       |                                        "m" : "check 3 is compliant"
       |                                      }
       |                                    ]
@@ -388,9 +382,7 @@ object ExpectedJson {
       |                                    "rtid" : "reportId-check2",
       |                                    "msrs" : [
       |                                      {
-      |                                        "rt" : {
-      |                                          "EnforceError" : {}
-      |                                        },
+      |                                        "rt" : "EnforceError",
       |                                        "m" : "check 2 failed"
       |                                      }
       |                                    ]
@@ -401,15 +393,11 @@ object ExpectedJson {
       |                                    "rtid" : "reportId-check1",
       |                                    "msrs" : [
       |                                      {
-      |                                        "rt" : {
-      |                                          "EnforceSuccess" : {}
-      |                                        },
+      |                                        "rt" : "EnforceSuccess",
       |                                        "m" : "check 1#1 is valid"
       |                                      },
       |                                      {
-      |                                        "rt" : {
-      |                                          "EnforceSuccess" : {}
-      |                                        },
+      |                                        "rt" : "EnforceSuccess",
       |                                        "m" : "check 1#2 is valid"
       |                                      }
       |                                    ]
@@ -447,9 +435,7 @@ object ExpectedJson {
       |                              "rtid" : "reportId-check6",
       |                              "msrs" : [
       |                                {
-      |                                  "rt" : {
-      |                                    "EnforceError" : {}
-      |                                  },
+      |                                  "rt" : "EnforceError",
       |                                  "m" : "check 6 is in error"
       |                                }
       |                              ]
@@ -460,9 +446,7 @@ object ExpectedJson {
       |                              "rtid" : "reportId-check5",
       |                              "msrs" : [
       |                                {
-      |                                  "rt" : {
-      |                                    "EnforceNotApplicable" : {}
-      |                                  },
+      |                                  "rt" : "EnforceNotApplicable",
       |                                  "m" : "check 5 is N/A"
       |                                }
       |                              ]
@@ -473,9 +457,7 @@ object ExpectedJson {
       |                              "rtid" : "reportId-check4",
       |                              "msrs" : [
       |                                {
-      |                                  "rt" : {
-      |                                    "EnforceRepaired" : {}
-      |                                  },
+      |                                  "rt" : "EnforceRepaired",
       |                                  "m" : "check 4 is repaired"
       |                                }
       |                              ]
