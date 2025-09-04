c64dec7ce1958005eef775b5caf404a54c968d9b
Clark Andrianasolo <clark.andrianasolo@rudder.io>
2025-09-04 14:48:48 +0200

    fixup! Fixes #27384: Missleading format for parameter category in API

--- webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/apidata/JsonQueryObjects.scala
+++ webapp/sources/rudder/rudder-core/src/main/scala/com/normation/rudder/apidata/JsonQueryObjects.scala
@@ -253,21 +253,21 @@ object JsonQueryObjects {
   }
 
   final case class JQRule(
-      id:               Option[RuleId] = None,
-      displayName:      Option[String] = None,
-      category:         Option[String] = None,
-      shortDescription: Option[String] = None,
-      longDescription:  Option[String] = None,
-      directives:       Option[Set[DirectiveId]] = None,
-      targets:          Option[Set[JRRuleTarget]] = None,
-      enabled:          Option[Boolean] = None,
-      tags:             Option[Tags] = None, // for clone
+      id:                                  Option[RuleId] = None,
+      displayName:                         Option[String] = None,
+      @jsonAliases("category") categoryId: Option[String] = None,
+      shortDescription:                    Option[String] = None,
+      longDescription:                     Option[String] = None,
+      directives:                          Option[Set[DirectiveId]] = None,
+      targets:                             Option[Set[JRRuleTarget]] = None,
+      enabled:                             Option[Boolean] = None,
+      tags:                                Option[Tags] = None, // for clone
 
       source: Option[RuleId] = None
   ) {
 
     val onlyName: Boolean = displayName.isDefined &&
-      category.isEmpty &&
+      categoryId.isEmpty &&
       shortDescription.isEmpty &&
       longDescription.isEmpty &&
       directives.isEmpty &&
@@ -278,7 +278,7 @@ object JsonQueryObjects {
     def updateRule(rule: Rule): Rule = {
       val updateRevision   = id.map(_.rev).getOrElse(rule.id.rev)
       val updateName       = displayName.getOrElse(rule.name)
-      val updateCategory   = category.map(RuleCategoryId.apply).getOrElse(rule.categoryId)
+      val updateCategory   = categoryId.map(RuleCategoryId.apply).getOrElse(rule.categoryId)
       val updateShort      = shortDescription.getOrElse(rule.shortDescription)
       val updateLong       = longDescription.getOrElse(rule.longDescription)
       val updateDirectives = directives.getOrElse(rule.directiveIds)
