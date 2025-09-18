dfe48a28a7624ec706147fa291a47c6f68907052
Fdall <felix.dallidet@rudder.io>
2025-09-18 19:24:46 +0200

    fixup! fixup! Fixes #27585: Test the command_execution_options generic method
    Fixes #27585: Test the command_execution_options generic method

--- policies/lib/tests/integration/command_execution_options_test.rs
+++ policies/lib/tests/integration/command_execution_options_test.rs
@@ -253,3 +253,179 @@ fn it_returns_an_error_if_the_timeout_is_exceeded() {
     r.assert_log_v4_result_conditions(tested_method, MethodStatus::Error);
     end_test(workdir);
 }
+#[test]
+fn it_errors_if_args_is_malformed() {
+    let workdir = init_test();
+    let file_path = workdir.path().join("target.txt");
+
+    let tested_method = &method(
+        "command_execution_options",
+        &[
+            "touch",
+            file_path.to_str().unwrap(),
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            &get_current_uid().to_string(),
+            &get_current_gid().to_string(),
+            "",
+            "",
+            "true",
+        ],
+    )
+    .enforce();
+    let r = MethodTestSuite::new()
+        .when(tested_method)
+        .execute(get_lib_path(), workdir.path().to_path_buf());
+    r.assert_legacy_result_conditions(tested_method, vec![MethodStatus::Error]);
+    r.assert_log_v4_result_conditions(tested_method, MethodStatus::Error);
+
+    assert!(
+        !file_path.exists(),
+        "File should not have been create by the method execution"
+    );
+    end_test(workdir);
+}
+
+#[test]
+fn it_parses_multiple_args_parameter() {
+    let workdir = init_test();
+    let file_path = workdir.path().join("target.txt");
+    let file_path2 = workdir.path().join("target2.txt");
+
+    let tested_method = &method(
+        "command_execution_options",
+        &[
+            "touch",
+            &format!(
+                r#"[ "{}", "{}"]"#,
+                file_path.to_str().unwrap(),
+                file_path2.to_str().unwrap()
+            ),
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            &get_current_uid().to_string(),
+            &get_current_gid().to_string(),
+            "",
+            "",
+            "true",
+        ],
+    )
+    .enforce();
+    let r = MethodTestSuite::new()
+        .when(tested_method)
+        .execute(get_lib_path(), workdir.path().to_path_buf());
+    r.assert_legacy_result_conditions(tested_method, vec![MethodStatus::Repaired]);
+    r.assert_log_v4_result_conditions(tested_method, MethodStatus::Repaired);
+
+    assert!(
+        file_path.exists(),
+        "File should have been create by the method execution"
+    );
+    assert!(
+        file_path2.exists(),
+        "File should have been create by the method execution"
+    );
+    end_test(workdir);
+}
+#[test]
+fn it_fails_when_using_the_args_parameter_and_the_use_shell_parameter() {
+    let workdir = init_test();
+    let file_path = workdir.path().join("target.txt");
+
+    let tested_method = &method(
+        "command_execution_options",
+        &[
+            "touch",
+            file_path.to_str().unwrap(),
+            "",
+            "true",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            &get_current_uid().to_string(),
+            &get_current_gid().to_string(),
+            "",
+            "",
+            "true",
+        ],
+    )
+    .enforce();
+    let r = MethodTestSuite::new()
+        .when(tested_method)
+        .execute(get_lib_path(), workdir.path().to_path_buf());
+    r.assert_legacy_result_conditions(tested_method, vec![MethodStatus::Error]);
+    r.assert_log_v4_result_conditions(tested_method, MethodStatus::Error);
+
+    assert!(
+        !file_path.exists(),
+        "File should not have been create by the method execution"
+    );
+    end_test(workdir);
+}
+#[test]
+fn it_uses_the_given_env_vars() {
+    let workdir = init_test();
+    let file_path = workdir.path().join("target.txt");
+
+    let tested_method = &method(
+        "command_execution_options",
+        &[
+            &format!(r#"echo "$FOO">"{}""#, file_path.to_str().unwrap()),
+            "",
+            "",
+            "true",
+            "",
+            workdir.path().to_str().unwrap(),
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            &get_current_uid().to_string(),
+            &get_current_gid().to_string(),
+            "",
+            r#"{ "FOO": "foo", "BAR": "bar" }"#,
+            "true",
+        ],
+    )
+    .enforce();
+    let r = MethodTestSuite::new()
+        .when(tested_method)
+        .execute(get_lib_path(), workdir.path().to_path_buf());
+    r.assert_legacy_result_conditions(tested_method, vec![MethodStatus::Repaired]);
+    r.assert_log_v4_result_conditions(tested_method, MethodStatus::Repaired);
+
+    let file_content = std::fs::read_to_string(file_path).unwrap();
+    assert_eq!(
+        file_content.trim(),
+        "foo",
+        "File content does not match the expected output"
+    );
+    end_test(workdir);
+}
