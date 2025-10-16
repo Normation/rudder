3de2e29669338b1654c0ce8d275851c539007a1d
Clark Andrianasolo <clark.andrianasolo@rudder.io>
2025-10-16 14:59:21 +0200

    Fixes #27731: Endpoint name are not unique cause /info API endpoint to be inexhaustive

--- webapp/sources/rudder/rudder-rest/src/test/scala/com/normation/rudder/rest/TestRestFromFileDef.scala
+++ webapp/sources/rudder/rudder-rest/src/test/scala/com/normation/rudder/rest/TestRestFromFileDef.scala
@@ -92,7 +92,7 @@ class TestRestFromFileDef extends ZIOSpecDefault {
                yamlSourceDirectory,
                yamlDestTmpDirectory,
                restTestSetUp.liftRules,
-               Nil,
+               List("api_info.yml"),
                transformations
              )
         _ <- effectUioUnit(
