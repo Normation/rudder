a5e8325acbfe2280ef472e5e3654c0e1d8fc83da
Alexis Mousset <alexis.mousset@rudder.io>
2025-11-15 19:05:52 +0100

    Fixes #27889: Missing details in minijinja  rendering errors

--- policies/rudder-cli/src/lib.rs
+++ policies/rudder-cli/src/lib.rs
@@ -3,6 +3,9 @@
 
 pub mod logs;
 
+use ariadne::{Config, IndexType, Label, Report, ReportKind, Source};
+use std::io::IsTerminal;
+use std::ops::Range;
 use std::panic;
 use std::process::exit;
 
@@ -28,4 +31,94 @@ pub fn custom_panic_hook_ignore_sigpipe() {
         // Hook back to default.
         (hook)(panic_info)
     }));
+}
+
+#[derive(Debug, Clone)]
+pub enum FileRange {
+    Byte(Range<usize>),
+    Char(Range<usize>),
+}
+
+/// Represents a range in a file
+///
+/// It can be either a byte range or a character range depending on its source.
+impl FileRange {
+    fn into_ariadne_range(self) -> (IndexType, Range<usize>) {
+        match self {
+            FileRange::Byte(r) => (IndexType::Byte, r),
+            FileRange::Char(r) => (IndexType::Char, r),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputType {
+    Report,
+    Terminal,
+}
+
+/// Compiler-like error reporting using ariadne
+///
+/// Example:
+///
+/// ```
+/// Error: Check error
+//    ╭─[ /etc/hosts:1:1 ]
+//    │
+//  1 │ 192.168.215.135 lists.normation.com
+//    │ ───────┬───────
+//    │        ╰───────── IP is not in allowed range: 10.0.0.0/16
+//    │
+//    │ Note: This is a note
+// ───╯
+/// ```
+pub fn format_report(
+    title: &str,
+    message: &str,
+    range: FileRange,
+    file_name: &str,
+    file_content: &str,
+    note: Option<&str>,
+) -> String {
+    let (index_type, range) = range.into_ariadne_range();
+    let span = (file_name, range);
+    let is_terminal = std::io::stdout().is_terminal();
+
+    let mut report = Report::build(ReportKind::Error, span.clone())
+        .with_config(
+            Config::default()
+                .with_color(false)
+                .with_index_type(index_type)
+                .with_color(is_terminal)
+                .with_compact(!is_terminal),
+        )
+        .with_message(title)
+        .with_label(Label::new(span).with_message(message));
+    if let Some(n) = note {
+        report = report.with_note(n);
+    }
+    let report = report.finish();
+    let source = Source::from(file_content);
+    let mut out = vec![];
+    report.write((file_name, source), &mut out).unwrap();
+    String::from_utf8_lossy(&out).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_inline_check_error() {
+        let report = format_report(
+            "Check error",
+            "IP is not in allowed range: 10.0.0.0/16",
+            FileRange::Char(0..15),
+            "/etc/hosts",
+            "192.168.215.135 lists.normation.com\n192.168.215.12 mail.normation.com",
+            Some("This is a note"),
+        );
+        let output = include_str!("../tests/report.log");
+        assert_eq!(report, output);
+    }
 }
