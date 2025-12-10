4ac90474c7aaee0fe116555644e841c570246892
pauline.iogna <pauline.iogna@gmail.com>
2025-12-10 14:36:28 +0100

    fixup! Fixes #27741: pair programming with mathieu
    Fixes #27741: Test to compare the json ecoding of zio-json and lift-json

--- webapp/sources/rudder/rudder-core/src/test/scala/com/normation/rudder/LiftJson.scala
+++ webapp/sources/rudder/rudder-core/src/test/scala/com/normation/rudder/LiftJson.scala
@@ -1,3 +1,49 @@
+/*
+ *************************************************************************************
+ * Copyright 2020 Normation SAS
+ *************************************************************************************
+ *
+ * This file is part of Rudder.
+ *
+ * Rudder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In accordance with the terms of section 7 (7. Additional Terms.) of
+ * the GNU General Public License version 3, the copyright holders add
+ * the following Additional permissions:
+ * Notwithstanding to the terms of section 5 (5. Conveying Modified Source
+ * Versions) and 6 (6. Conveying Non-Source Forms.) of the GNU General
+ * Public License version 3, when you create a Related Module, this
+ * Related Module is not considered as a part of the work and may be
+ * distributed under the license agreement of your choice.
+ * A "Related Module" means a set of sources files including their
+ * documentation that, without modification of the Source Code, enables
+ * supplementary functions or services in addition to those offered by
+ * the Software.
+ *
+ * Rudder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rudder.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ *************************************************************************************
+ */
+
+/**
+ * Migration lift-json to zio-json.
+ * MigrationLiftjson2Ziojson
+ * 
+ * This code is made to facilitate the migration from lift-json to zio-json.
+ * - All files created for the migration should have a comment description containing the key word MigrationLiftjson2Ziojson.
+ * - All files created for the migration should be delete after the migration
+ * - All having the key word MigrationLiftjson2Ziojson should be delete after the migration
+ */
 package com.normation.rudder
 
 trait LiftJson[A] {
