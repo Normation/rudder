8cb37bf90697f3035102a922d409803cfa319ae8
Fdall <felix.dallidet@rudder.io>
2026-02-06 17:03:25 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! Fixes #28266: Add a run-module command to the rudder-moddule-system-update CLI
    Fixes #28266: Add a run-module command to the rudder-moddule-system-update CLI

--- policies/module-types/system-updates/src/package_manager/windows_update_agent/update/download_result.rs
+++ policies/module-types/system-updates/src/package_manager/windows_update_agent/update/download_result.rs
@@ -3,15 +3,17 @@
 
 use super::{Collection, InfoData, OperationResultCode};
 use crate::package_manager::PackageId;
-use anyhow::{Error, Result, bail};
+use crate::package_manager::windows_update_agent::update::rudder_hresult::RudderHRESULT;
+use anyhow::{Context, Error, Result, bail};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt::Display;
 use windows::Win32::System::UpdateAgent::{IDownloadResult, IUpdateDownloadResult};
+use windows::core::HRESULT;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct UpdateDownloadResult {
-    pub h_result: i32,
+    pub h_result: RudderHRESULT,
     pub result_code: OperationResultCode,
     pub update: InfoData,
 }
@@ -21,23 +23,33 @@ impl UpdateDownloadResult {
         r: IUpdateDownloadResult,
         u: InfoData,
     ) -> Result<UpdateDownloadResult, Error> {
-        unsafe {
-            Ok(Self {
-                h_result: r.HResult()?,
-                result_code: OperationResultCode::new(r.ResultCode()?.0)?,
-                update: u,
-            })
-        }
+        let h_result = unsafe {
+            RudderHRESULT::from(HRESULT(
+                r.HResult()
+                    .context("Could not retrieve the download operation HRESULT code")?,
+            ))
+        };
+        let result_code = unsafe {
+            let rc: i32 = r
+                .ResultCode()
+                .context("Could not retrieve the download operation result code")?
+                .0;
+            OperationResultCode::new(rc).context(format!(
+                "Could not instantiate the OperationResultCode object from {}",
+                rc
+            ))?
+        };
+        Ok(Self {
+            h_result,
+            result_code,
+            update: u,
+        })
     }
 }
 
 impl Display for UpdateDownloadResult {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "h_result: {}, result_code: {}",
-            self.h_result, self.result_code
-        )
+        write!(f, "result_code: {}, {}", self.result_code, self.h_result)
     }
 }
 
@@ -61,7 +73,7 @@ impl DownloadResult {
         for i in 0..collection.len() {
             h.insert(
                 PackageId::from(collection[i].data.clone()),
-                format!("\nInstall result:\n{}", self.update_results[i].clone()),
+                format!("\nDownload result:\n  - {}", self.update_results[i].clone()),
             );
         }
         Ok(h)
