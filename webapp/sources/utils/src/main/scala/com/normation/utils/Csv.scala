dc412ef588893b684e79be460ceb6bbc27fc9f8c
Clark Andrianasolo <clark.andrianasolo@rudder.io>
2026-02-09 12:23:41 +0100

    Fixes #28301: Add CSV Scala utils in rudder

--- webapp/sources/utils/src/main/scala/com/normation/utils/Csv.scala
+++ webapp/sources/utils/src/main/scala/com/normation/utils/Csv.scala
@@ -37,6 +37,8 @@
 
 package com.normation.utils
 
+import cats.data.NonEmptyList
+import cats.syntax.semigroupk.*
 import enumeratum.EnumEntry
 import java.time.ZonedDateTime
 import magnolia1.CaseClass
@@ -53,6 +55,7 @@ trait Csv[A] {
 }
 
 object Csv extends ProductDerivation[Csv] {
+
   import org.apache.commons.csv.*
   import org.apache.commons.lang3.StringUtils
 
@@ -60,40 +63,104 @@ object Csv extends ProductDerivation[Csv] {
 
   /**
    * Proof of header for CSV instances.
-   * The header for a CSV field is always the case class field name, formatted from camelCase to it's human readable version.
-   * Therefore using a type class instance to customize the header will not work.
+   * Single column headers should inherit the [[Header.One]] subtype.
+   * For custom column names, the [[Header.Const]] subtype should be used.
    */
   trait Header[A] {
-    def header: List[String] = Nil
+    private[Csv] def header: Headers
   }
 
-  object Header extends ProductDerivation[Header] {
-    // Derive headers names using the field names, an empty (default) header means a fresh column should be added from field
-    def join[A](ctx: CaseClass[Header, A]): Header[A] = new Header[A] {
-      override def header: List[String] = {
-        ctx.parameters
-          .flatMap(p => {
-            val fields = p.typeclass.header
-            if (fields.isEmpty) p.label :: Nil
-            else fields
-          })
-          .map(field => StringUtils.capitalize(StringUtils.splitByCharacterTypeCamelCase(field).mkString(" ")))
-          .toList
+  /*
+   * Headers are computed such that : an empty one means "1 column, with the name of a case class field".
+   * When headers are getting combined when deriving an ADT, the NEL is useful for combining values
+   */
+  opaque type Headers = OneColumnHeader | NonEmptyList[String]
+
+  /*
+   * Single columns headers are empty at first during derivation, but are enforced to contain a value upon derivation
+   */
+  opaque type OneColumnHeader = Option[String]
+
+  private[Csv] object Headers {
+    def apply(values: NonEmptyList[String]): Headers = values
+
+    def one(value: String): Headers = Some(value)
+
+    def default: Headers = None
+
+    extension (self: Headers) {
+      def value: List[String] = self match {
+        case None    => Nil
+        case Some(h) => h :: Nil
+        case nel: NonEmptyList[?] => nel.toList
       }
     }
 
-    given headerOption[A: Header]: Header[Option[A]] with { override def header: List[String] = summon[Header[A]].header }
+    extension (list: List[Headers]) {
+      def flattenHeaders: Headers = list.fold(None) {
+        case (a: OneColumnHeader, b: OneColumnHeader)       =>
+          (NonEmptyList.fromFoldable(a) <+> NonEmptyList.fromFoldable(b)).getOrElse(None)
+        case (nel: NonEmptyList[?], o: OneColumnHeader)     => nel.appendList(o.toList)
+        case (o: OneColumnHeader, nel: NonEmptyList[?])     => nel.prependList(o.toList)
+        case (nel1: NonEmptyList[?], nel2: NonEmptyList[?]) => nel1.concatNel(nel2)
+      }
+    }
+  }
+
+  object Header extends ProductDerivation[Header] {
+
+    import Headers.*
+
+    extension [A](self: Header[A]) {
+      /*
+       * This is the logic of headers for type-class derivation :
+       * - constant/explicit headers should never be overridden
+       * - when the default "one column header" instance is used, the name of the column is needed
+       *   - it should be camelCase and replaced into PascalCase separated with a space
+       */
+      private[Csv] def apply(value: => String): Headers = self match {
+        case c: Const[?] => c.header // never override a const header
+        case _ =>
+          self.header match {
+            case h:   OneColumnHeader =>
+              Headers.one(StringUtils.capitalize(StringUtils.splitByCharacterTypeCamelCase(value).mkString(" ")))
+            case nel: NonEmptyList[?] =>
+              Headers(nel)
+          }
+      }
+    }
+
+    trait Const[A](values: NonEmptyList[String]) extends Header[A] {
+      override def header: Headers = Headers(values)
+    }
+
+    trait One[A] extends Header[A] {
+      override def header: Headers = Headers.default
+    }
+
+    // Derive headers names using the field names, an empty (default) header means a fresh column should be added from field
+    def join[A](ctx: CaseClass[Header, A]): Header[A] = new Header[A] {
+      override def header: Headers = {
+        ctx.parameters.toList.map(p => p.typeclass(p.label)).flattenHeaders
+      }
+    }
+
+    given headerOption[A: Header]: Header[Option[A]] with {
+      override def header: Headers = summon[Header[A]].header
+    }
 
     // utility instances for known sum types, since their header can only be guessed from the corresponding field name in the case class
-    given headerEnumEntry[A <: EnumEntry]: Header[A] with {}
+    given headerEnumEntry[A <: EnumEntry]: Header.One[A] with {}
 
     // instance that must exist, but that make no sense unless the `deriveHeader` macro is used on a case class (derives Header)
-    given headerAnyVal[A <: AnyVal]: Header[A] with {}
+    given headerAnyVal[A <: AnyVal]: Header.One[A] with {}
 
     // custom, one-column instances
-    given headerString:        Header[String] with        {}
-    given headerDateTime:      Header[DateTime] with      {}
-    given headerZonedDateTime: Header[ZonedDateTime] with {}
+    given headerString: Header.One[String] with {}
+
+    given headerDateTime: Header.One[DateTime] with {}
+
+    given headerZonedDateTime: Header.One[ZonedDateTime] with {}
   }
 
   def join[A](ctx: CaseClass[Csv, A]): Csv[A] =
@@ -119,7 +186,7 @@ object Csv extends ProductDerivation[Csv] {
       val header   = summon[Header[A]].header
       val instance = summon[Csv[A]]
       val out      = new StringBuilder()
-      csvFormat.printRecord(out.underlying, header*)
+      csvFormat.printRecord(out.underlying, header.value*)
       results.foreach(l => csvFormat.printRecord(out.underlying, instance.apply(l)*))
       out.toString
     }
