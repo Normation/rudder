b300718385ebdceeb6e72a5764d470b3fac2df21
Raphael Gauthier <raphael.gauthier@normation.com>
2026-03-02 16:31:29 +0100

    Fixes #28453: HTML in tooltips is no longer rendered

--- webapp/sources/rudder/rudder-web/src/main/elm/sources/Utils/TooltipUtils.elm
+++ webapp/sources/rudder/rudder-web/src/main/elm/sources/Utils/TooltipUtils.elm
@@ -2,27 +2,17 @@ module Utils.TooltipUtils exposing (..)
 
 -- WARNING:
 --
--- Here we are building an html snippet that will be placed inside an attribute, so
--- we can't easily use the Html type as there is no built-in way to serialize it manually.
--- This means it will be vulnerable to XSS on its parameters (here the description).
+-- This should only be used to fill the 'title' HTML attribute with Bootstrap's tooltip/popover system,
+-- as it already sanitises the content of this attribute by default.
+-- Parameters 'title' and 'content' must be sanitized manually if used in another scenario.
 --
--- We resort to escaping it manually here.
+
 buildTooltipContent : String -> String -> String
 buildTooltipContent title content =
   let
     headingTag = "<h4 class='tags-tooltip-title'>"
     contentTag = "</h4><div class='tooltip-inner-content'>"
     closeTag   = "</div>"
-    escapedTitle = htmlEscape title
-    escapedContent = htmlEscape content
   in
-    headingTag ++ escapedTitle ++ contentTag ++ escapedContent ++ closeTag
-
-htmlEscape : String -> String
-htmlEscape s =
-  String.replace "&" "&amp;" s
-    |> String.replace ">" "&gt;"
-    |> String.replace "<" "&lt;"
-    |> String.replace "\"" "&quot;"
-    |> String.replace "'" "&#x27;"
-    |> String.replace "\\" "&#x2F;"
+    headingTag ++ title ++ contentTag ++ content ++ closeTag
+    
